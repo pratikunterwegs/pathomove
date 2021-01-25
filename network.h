f49e5e40696709fffc54d613913294f516a68428
Pratik Gupte <pratikgupte16@gmail.com>
2021-01-25 21:50:56 +0100

    update network

--- network.h
+++ network.h
@@ -2,11 +2,13 @@
 #define NETWORK_H
 
 #include <vector>
+#include <cassert>
 
 struct Network {
 public:
     Network():
-        nAgents(100)
+        nAgents(100),
+        associations(nAgents - 1)
     {}
     ~Network() {}
 
@@ -20,9 +22,12 @@ public:
 void Network::initAssociations(int nVertices){
 
     nAgents = nVertices;
-    for(size_t i = 0; nAgents - (static_cast<int>(i) + 1) > 0; i ++) {
-        associations.push_back(std::vector<int> (nAgents - (static_cast<int>(i) + 1), 0));
+    for(size_t i = 0; i < (nAgents - 1); i ++) {
+        associations[i] = (std::vector<int> (nAgents - (i + 1), 0));
     }
+
+    // check size
+    assert(associations[0].size() == (nAgents - 1) && "association triangle is wrong");
 }
 
 #endif // NETWORK_H
