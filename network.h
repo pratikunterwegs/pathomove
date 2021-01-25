dc17616fffd69b423d655e029d42925389a21393
Pratik Gupte <pratikgupte16@gmail.com>
2021-01-25 16:41:33 +0100

    add network

---
+++ network.h
@@ -0,0 +1,26 @@
+#ifndef NETWORK_H
+#define NETWORK_H
+
+#include <vector>
+
+struct Network {
+public:
+    Network():
+        nAgents(100)
+    {}
+    ~Network() {}
+
+    int nAgents;
+    std::vector<std::vector<int> > associations;
+
+    void initAssociations();
+};
+
+// initialise associations
+void Network::initAssociations(){
+    for(size_t i = 0; nAgents - (static_cast<int>(i) + 1) > 0; i ++) {
+        associations.push_back(std::vector<int> (nAgents - (static_cast<int>(i) + 1), 0));
+    }
+}
+
+#endif // NETWORK_H
