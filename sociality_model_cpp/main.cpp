e4436a8aa132b4d1468b77debd1f0fc03718be2e
Pratik Gupte <pratikgupte16@gmail.com>
2021-01-26 15:42:32 +0100

    landscape size as cli

--- sociality_model_cpp/main.cpp
+++ sociality_model_cpp/main.cpp
@@ -18,8 +18,10 @@ int main(int argc, char *argv[])
     const int tmax = std::stoi(cliArgs[2]);
     const int clusters = std::stoi(cliArgs[3]);
     const double dispersal = std::stod(cliArgs[4]);
+    const double landsize = std::stod(cliArgs[5]);
     //        std::string rep = cliArgs[5];a
-    do_simulation(genmax, tmax, clusters, dispersal);
+//    do_simulation(genmax, tmax, clusters, dispersal);
+    export_test_landscapes(clusters, dispersal, 3, landsize);
 
     return 0;
 
