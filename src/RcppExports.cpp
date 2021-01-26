9d89c609501d44b861e8da26ba9d6c1d32e95b5b
Pratik Gupte <pratikgupte16@gmail.com>
2021-01-26 14:55:51 +0100

    rcpp new fun

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,18 @@
 
 using namespace Rcpp;
 
+// export_test_landscapes
+void export_test_landscapes(int foodClusters, double clusterDispersal, int replicates);
+RcppExport SEXP _socialitymodel_export_test_landscapes(SEXP foodClustersSEXP, SEXP clusterDispersalSEXP, SEXP replicatesSEXP) {
+BEGIN_RCPP
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< int >::type foodClusters(foodClustersSEXP);
+    Rcpp::traits::input_parameter< double >::type clusterDispersal(clusterDispersalSEXP);
+    Rcpp::traits::input_parameter< int >::type replicates(replicatesSEXP);
+    export_test_landscapes(foodClusters, clusterDispersal, replicates);
+    return R_NilValue;
+END_RCPP
+}
 // do_simulation
 void do_simulation(int genmax, int tmax, int foodClusters, double clusterDispersal);
 RcppExport SEXP _socialitymodel_do_simulation(SEXP genmaxSEXP, SEXP tmaxSEXP, SEXP foodClustersSEXP, SEXP clusterDispersalSEXP) {
@@ -20,6 +32,7 @@ END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
+    {"_socialitymodel_export_test_landscapes", (DL_FUNC) &_socialitymodel_export_test_landscapes, 3},
     {"_socialitymodel_do_simulation", (DL_FUNC) &_socialitymodel_do_simulation, 4},
     {NULL, NULL, 0}
 };
