906b5689dbd836ed40627a0e6c40841094bd48dc
Pratik Gupte <pratikgupte16@gmail.com>
2021-01-26 10:39:49 +0100

    add sim function

--- simulations.cpp
+++ simulations.cpp
@@ -1,71 +1,120 @@
+#include <vector>
+#include <random>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include "parameters.h"
+#include "landscape.h"
+#include "agents.h"
+#include "network.h"
 
-//// init food
-//Resources food;
-//food.initResources(30, 1.5);
-//food.countAvailable();
+//        if(gen % 10 == 0) {
+//            std::cout << "gen = " << gen << "\n";
 
-////    std::cout << food.nAvailable << "\n";
-
-//// print to file
-//std::ofstream ofs;
-//ofs.open("items.csv", std::ofstream::out);
-//ofs << "x,y\n";
-
-//for (size_t i = 0; i < static_cast<size_t>(food.nItems); i++)
-//{
-//    ofs << food.coordX[i] << "," << food.coordY[i] << "\n";
-//}
-
-//ofs.close();
-
-//// init pop
-//Population pop;
-//pop.initPos(food);
-//pop.setTrait();
-
-//std::ofstream moveofs;
-//moveofs.open("moves.csv", std::ofstream::out);
-//moveofs << "id,gen,x,y,energy,trait\n";
-
-//for(int gen = 0; gen < 10500; gen++) {
-
-
-//    std::cout << "gen = " << gen << "\n";
-
-//    for (size_t t = 0; t < 100; t++) {
-
-//        pop.initPos(food);
-//        pop.move(food);
-
-//        for (size_t i = 0; i < static_cast<size_t>(pop.nAgents); i++)
-//        {
-//            forage(i, food, pop);
-//            food.countAvailable();
+//            // print evolved pop
+////            for(size_t i = 0; i < static_cast<size_t>(pop.nAgents); i++){
+////                std::cout << i << "\n";
+////                traitofs << i << ","
+////                        << gen << ","
+//////                        << pop.coordX[i] << ","
+//////                        << pop.coordY[i] << ","
+//////                        << pop.energy[i] << ","
+////                        << pop.trait[i] << "\n";
+////            }
 //        }
 
-//        // decrement food counter by one
-//        for (size_t j = 0; j < static_cast<size_t>(food.nItems); j++){
-//            if(food.counter[j] > 0) {
-//                food.counter[j] --;
+//if(gen > (genmax -10)) {
+
+//    // print pbsn
+//    for(size_t i = 0; i < static_cast<size_t>(pop.nAgents - 1); i++) {
+//        for(size_t j = 0; j < pop.pbsn.associations[i].size(); j++) {
+
+//            if(pop.pbsn.associations[i][j] > 0) {
+//                pbsnofs << gen << ","
+//                        << i << ","
+//                        << j + 1 << ","
+//                        << pop.pbsn.associations[i][j] << "\n";
 //            }
 
 //        }
 //    }
-
-//    if(gen % 10 == 0) {
-//        // print evolved pop
-//        for(size_t i = 0; i < pop.nAgents; i++){
-//            moveofs << i << ","
-//                    << gen << ","
-//                    << pop.coordX[i] << ","
-//                    << pop.coordY[i] << ","
-//                    << pop.energy[i] << ","
-//                    << pop.trait[i] << "\n";
-//        }
-//    }
-//    // reproduce
-//    pop.Reproduce();
-
 //}
 
-//moveofs.close();
+//void export_landscape()
+//{
+//    // print to file
+//    std::ofstream ofs;
+//    ofs.open("items.csv", std::ofstream::out);
+//    ofs << "x,y\n";
+
+//    for (size_t i = 0; i < static_cast<size_t>(food.nItems); i++)
+//    {
+//        ofs << food.coordX[i] << "," << food.coordY[i] << "\n";
+//    }
+
+//    ofs.close();
+//}
+
+//void export_traits() {
+//    // position ofs
+//    std::ofstream traitofs;
+//    traitofs.open("trait.csv", std::ofstream::out);
+//    traitofs << "id,gen,trait\n";
+//}
+
+//void export_pbsn(){
+//    // pbsn ofs
+//    std::ofstream pbsnofs;
+//    pbsnofs.open("pbsn.csv", std::ofstream::out);
+//    pbsnofs << "gen,id1,id2,weight\n";
+
+//    std::cout << "opened ofs\n";
+//}
+
+void do_simulation(int genmax, int tmax, int foodClusters, double clusterDispersal)
+{
+    // set seed
+    gsl_rng_set(r, seed);
+    // init food
+    Resources food;
+    food.initResources(foodClusters, clusterDispersal);
+    food.countAvailable();
+
+    /// export landscape
+
+    // init pop
+    Population pop;
+    pop.setTrait();
+
+    for(int gen = 0; gen < genmax; gen++) {
+
+        pop.initPos(food);
+        pop.pbsn.initAssociations(pop.nAgents);
+        for (int t = 0; t < tmax; t++) {
+
+            pop.move(food);
+            pop.updatePbsn();
+
+
+            for (size_t i = 0; i < static_cast<size_t>(pop.nAgents); i++)
+            {
+                forage(i, food, pop);
+                food.countAvailable();
+            }
+
+            //decrement food counter by one
+            for (size_t j = 0; j < static_cast<size_t>(food.nItems); j++)
+            {
+                if(food.counter[j] > 0) {
+                    food.counter[j] --;
+                }
+            }
+            // timestep ends here
+        }
+        // generation ends here
+        // reproduce
+        pop.Reproduce();
+
+    }
+    std::cout << "done";
+}
