a04887515b248192c930aa61af430aa07dd9d6de
Pratik Gupte <pratikgupte16@gmail.com>
2021-03-09 19:09:40 +0100

    init network flex size

--- src/network.h
+++ src/network.h
@@ -6,13 +6,8 @@
 
 struct Network {
 public:
-    Network():
-        nAgents(100),
-        associations(nAgents - 1)
-    {}
-    ~Network() {}
 
-    int nAgents;
+    int nAgents = 0;
     std::vector<std::vector<int> > associations;
 
     void initAssociations(int nVertices);
@@ -22,6 +17,7 @@ public:
 void Network::initAssociations(int nVertices){
 
     nAgents = nVertices;
+    associations = std::vector<std::vector<int> > (nAgents - 1, std::vector<int> (nAgents - 1, 0));
     for(size_t i = 0; i < static_cast<size_t>(nAgents - 1); i ++) {
         associations[i] = (std::vector<int> (nAgents - (i + 1), 0));
     }
