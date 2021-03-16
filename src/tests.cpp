911a4b686ced8db607464b7caee2a8f75e8c37d6
Pratik Gupte <pratikgupte16@gmail.com>
2021-03-16 10:57:56 +0100

    no tests here

--- src/tests.cpp
+++
@@ -1,8 +0,0 @@
-#include "agents.h"
-#define BOOST_TEST_MODULE MyTest
-#include <boost/test/unit_test.hpp>
-
-BOOST_AUTO_TEST_CASE(test_associations) {
-    // check associations have length n agents
-    BOOST_CHECK(pop.associations.size() == pop.nAgents);
-}
