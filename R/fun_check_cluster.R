97691e1c7be6045bcb20b80d0bf97ad4409c09fd
Pratik Gupte <pratikgupte16@gmail.com>
2021-03-19 19:08:42 +0100

    correct check fun again

--- R/fun_check_cluster.R
+++ R/fun_check_cluster.R
@@ -14,15 +14,17 @@ check_prepare_cluster <- function(
 
   # prepare check and fail case clone
   cluster_check <- glue::glue(
-   'if [[ -d "snevo" ]]; then
-       echo "snevo exists, updating"
-       cd snevo
-       git remote update
-       if [[ `git status --porcelain` ]]; then
+   'if [[ -d "snevo" ]] 
+    then
+      echo "snevo exists, updating"
+      cd snevo
+      git remote update
+      if [[ `git status --porcelain` ]]
+      then
           git pull
           chmod +x bash/install_snevo.sh
           ./bash/install_snevo.sh
-       fi
+      fi
     else 
        echo "snevo does not exist, cloning"
        git clone https://github.com/pratikunterwegs/snevo.git snevo
