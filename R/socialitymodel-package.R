f4e7ba86382dc712916eb33a3d14f50619e09edf
Pratik Gupte <pratikgupte16@gmail.com>
2021-03-19 13:08:55 +0100

    style

--- R/socialitymodel-package.R
+++ R/socialitymodel-package.R
@@ -1,7 +1,7 @@
 ## usethis namespace: start
-  #' @useDynLib socialitymodel, .registration = TRUE
-  ## usethis namespace: end
+#' @useDynLib socialitymodel, .registration = TRUE
+## usethis namespace: end
 
 ## usethis namespace: start
-  #' @importFrom Rcpp sourceCpp
-  ## usethis namespace: end
+#' @importFrom Rcpp sourceCpp
+## usethis namespace: end
