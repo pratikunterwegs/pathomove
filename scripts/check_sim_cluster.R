98c95f06cb7e0239d50993ad750d8dc1bf491329
Pratik Gupte <pratikgupte16@gmail.com>
2021-03-19 13:50:22 +0100

    check sim script

---
+++ scripts/check_sim_cluster.R
@@ -0,0 +1,45 @@
+args = commandArgs(TRUE)
+
+param_file = args[1]
+
+row_n = as.numeric(args[2])
+
+params = read.csv(param_file)
+
+# run simulation
+data_evolved_pop = snevo::do_simulation(
+  popsize = params$popsize[row_n],
+  genmax = params$genmax[row_n], 
+  tmax = params$tmax[row_n], 
+  foodClusters = params$foodClusters[row_n], 
+  clusterDispersal = params$clusterDispersal[row_n],
+  landsize = 100
+)
+
+# get params as named vector
+these_params = unlist(params[row_n,])
+
+# append list of params
+data_evolved_pop = append(
+  data_evolved_pop,
+  params = these_params
+)
+
+# name of rdata file
+output_file = Reduce(
+  paste,
+  mapply(
+    function(p, np) {
+      paste(np, p, sep = "_")
+    }, these_params, names(these_params)
+  )
+)
+output_file = glue::glue(
+  'data/output/{output_file}.Rdata'
+)
+
+# save
+save(
+  data_evolved_pop,
+  file = output_file
+)
