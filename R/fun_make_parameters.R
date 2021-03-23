8c0b6b094af7b06c56669c6a900c253af592d2b4
Pratik Gupte <pratikgupte16@gmail.com>
2021-03-23 14:26:30 +0100

    add nfood and landsize

--- R/fun_make_parameters.R
+++ R/fun_make_parameters.R
@@ -7,21 +7,28 @@
 #' @param genmax Max generation.
 #' @param foodClusters How many clusters.
 #' @param clusterDispersal How spread out food is.
+#' @param nFood N food items, integer.
+#' @param landsize Landscape size, double value.
 #'
 #' @return Writes a parameter file.
 #' @export
 #'
 make_parameter_file <- function(
-                                genmax = 1000,
-                                popsize = 1000,
-                                tmax = 100,
-                                foodClusters = seq(1, 10),
-                                clusterDispersal = seq(1, 5),
-                                replicates = 10,
-                                which_file = "this_file.csv") {
+  genmax = 1000,
+  popsize = 1000,
+  tmax = 100,
+  nFood = 3000,
+  landsize = 300,
+  foodClusters = seq(1, 10),
+  clusterDispersal = seq(1, 5),
+  replicates = 10,
+  which_file = "this_file.csv"
+) {
 
   # make combinations
   parameters <- data.table::CJ(
+    nFood,
+    landsize,
     genmax,
     popsize,
     tmax,
