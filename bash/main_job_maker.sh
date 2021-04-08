c28b435ca9517cf7716ebcc016e63b8dc0367ab2
Pratik Gupte <pratikgupte16@gmail.com>
2021-04-08 20:58:15 +0200

    allow for more time and mem

--- bash/main_job_maker.sh
+++ bash/main_job_maker.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --time=01:00:00
+#SBATCH --time=03:00:00
 #SBATCH --partition=regular
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --job-name=snevo_simulation
 #SBATCH --array=1-n_array
 #SBATCH --output=/home/p284074/snevo/data/output/snevo_output_%a.log
-#SBATCH --mem=3000
+#SBATCH --mem=5000
 
 pwd
 ml load R/4.0.0-foss-2020a
