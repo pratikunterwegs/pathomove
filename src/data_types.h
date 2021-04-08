3c81a0b09c785a39bde84493e9702e3680317e55
Pratik Gupte <pratikgupte16@gmail.com>
2021-04-08 15:01:14 +0200

    network data object

--- src/data_types.h
+++ src/data_types.h
@@ -8,6 +8,7 @@
 #include "landscape.h"
 #include "agents.h"
 #include "network.h"
+#include "network_operations.hpp"
 
 // define a struct holding a vector of data frames which holds generation wise data
 struct genData {
@@ -20,12 +21,33 @@ public:
     Rcpp::List getGenData ();
 };
 
+struct networkData {
+public:
+    std::vector<int> interactions;
+    std::vector<double> diameter;
+    std::vector<double> global_efficiency;
+    std::vector<int> gens;
+
+    void updateNetworkData(Population &pop, const int gen, Network &pbsn);
+    Rcpp::DataFrame getNetworkData();
+};
+
 // function to update gendata
 void genData::updateGenData (Population &pop, const int gen_) {
     // get pop data
     genEnergyVec.push_back(pop.energy);
     genTraitVec.push_back(pop.trait);
     gens.push_back(gen_);
+}
+
+void networkData::updateNetworkData(Population &pop, const int gen, Network &pbsn) {
+
+    std::vector<double> tmpNetworkData = networkMeasures(pbsn, pop);
+
+    interactions.push_back(tmpNetworkData[0]);
+    diameter.push_back(tmpNetworkData[1]);
+    global_efficiency.push_back(tmpNetworkData[2]);
+    gens.push_back(gen);
 }
 
 // function to return gen data as an rcpp list
@@ -44,4 +66,16 @@ Rcpp::List genData::getGenData() {
     );
 
     return dataToReturn;
+}
+
+// function to return network data as a DataFrame
+Rcpp::DataFrame networkData::getNetworkData() {
+    Rcpp::DataFrame networkData = Rcpp::DataFrame::create(
+                Named("gen") = gens,
+                Named("interactions") = interactions,
+                Named("diameter") = diameter,
+                Named("global_efficiency") = global_efficiency
+            );
+
+    return networkData;
 }
