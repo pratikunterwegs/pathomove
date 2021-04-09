55d4cf9a72d0c2a309554579b802cfa2fd0b3d28
Pratik Gupte <pratikgupte16@gmail.com>
2021-04-09 12:57:33 +0200

    code to check landscapes

--- scripts/check_land.R
+++ scripts/check_land.R
@@ -4,27 +4,42 @@ library(snevo)
 library(data.table)
 library(ggplot2)
 
-landscape = snevo::get_test_landscape(
-    nItems = 3000,
-    landsize = 1000,
-    nClusters = 100,
-    clusterDispersal = 2
+# make parameters
+params = CJ(
+    landsize = 100.0,
+    nItems = 500L,
+    nClusters = as.integer(2 ^ seq(4, 8, 1)),
+    clusterDispersal = 2 ^ seq(-3, 3, 1)
 )
 
-bin_size = 0.01 # 5% landscape size
-bin_size = bin_size * nrow(landscape)
-setDT(landscape)
-landscape[, c("x", "y") := list(
-    plyr::round_any(x, bin_size),
-    plyr::round_any(y, bin_size)
-)]
-landscape = landscape[,.N, by = c("x", "y")]
+data = copy(params)
+# make landscape
+landscape = Map(function(l, n, nc, cd) {
+    setDT(snevo::get_test_landscape(
+        nItems = n,
+        landsize = l,
+        nClusters = nc,
+        clusterDispersal = cd
+    ))
+}, data$landsize, data$nItems, data$nClusters, data$clusterDispersal)
 
-# plot landscape as tiles
-ggplot(landscape)+
-    geom_tile(
-        aes(x, y, fill = N)
+data$landscape = landscape
+data$code = seq(nrow(data))
+
+## unnest and plot
+data = data[,unlist(landscape, recursive = F),
+    by = c("nItems", "landsize", "nClusters", "clusterDispersal", "code")]
+
+## plot
+landscape_grid = ggplot(data)+
+    geom_point(
+        aes(x, y),
+        shape = 1
     )+
-    scale_fill_distiller(
-        palette = "Greens"
-    )
+    facet_grid(nClusters ~ clusterDispersal, 
+        labeller = label_both
+    )+
+    coord_equal()
+
+ggsave(plot = landscape_grid, 
+    filename = "figures/fig_landscape_characters_500items.png")
