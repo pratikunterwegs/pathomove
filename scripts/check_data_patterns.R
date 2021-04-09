225cfabd64efedd9894d72b9b4a525d14141999e
Pratik Gupte <pratikgupte16@gmail.com>
2021-04-09 14:00:26 +0200

    add data patterns code

--- scripts/check_data_patterns.R
+++ scripts/check_data_patterns.R
@@ -1,6 +1,7 @@
 # load libs
 library(data.table)
 library(ggplot2)
+library(colorspace)
 
 # list files and read
 file_list = list.files("data/output/", pattern = "Rdata", full.names = T)
@@ -28,8 +29,8 @@ data = lapply(file_list, function(l) {
     })
   )
   data_tmp = data.table::rbindlist(data_tmp)[, energy := NULL]
-  data_tmp$trait = tanh(data_tmp$trait * 20)
-  data_tmp$trait_round = plyr::round_any(data_tmp$trait, 0.01)
+  data_tmp$trait = log10(data_tmp$trait)
+  data_tmp$trait_round = plyr::round_any(data_tmp$trait, 0.02)
   data_summary = data_tmp[, .N, by = c("foodClusters", 
                                    "clusterDispersal", "replicate",
                                    "trait_round", "gen")]
@@ -38,38 +39,48 @@ data = lapply(file_list, function(l) {
 # bind and check
 data = rbindlist(data)
 head(data)
-# 
-# # count trait by replicate and dispersal
-# data[, trait_round := cut(trait, seq(0, max(trait), 0.01))]
-# data_summary = data[, .N, by = c("foodClusters", 
-#                                  "clusterDispersal", "replicate",
-#                                  "trait_round", "gen")]
+data[, prop := N / sum(N), by = c("replicate", "gen", 
+                                  "foodClusters", "clusterDispersal")]
 
-# # get low
-# data_summary$trait_round = stringi::stri_extract_last(
-#   data_summary$trait_round, regex = "[-0-9]+\\.\\d{2}"
-# )
-# data_summary$trait_round = as.numeric(data_summary$trait_round)
+# count morphs
+data_morphs = data[prop > 0.005, .N, by = c("foodClusters", "clusterDispersal",
+                                           "replicate", "gen")]
 
-# plot and check
-ggplot(data[gen %% 10 == 0 && trait_round < 2])+
-  geom_tile(
-    aes(gen, trait_round,
-        fill = N)
+# make plot of morphs
+plot_morphs = 
+  ggplot(data_morphs[gen %% 10 == 0])+
+  geom_path(
+    aes(gen, N,
+        group = interaction(replicate)),
+    colour = "steelblue"
   )+
+  scale_x_log10()+
+  theme_classic()+
   coord_cartesian(
-    ylim = c(0, 2)
+    xlim = c(10, 1000),
+    ylim = c(0, 50)
   )+
-  scale_fill_distiller(
-    palette = "Reds", direction = 1
+  theme(
+    strip.background = element_blank(),
+    strip.text = element_text(face = "italic", hjust = 0)
   )+
-  facet_wrap(~replicate, labeller = label_both)
+  facet_wrap(foodClusters ~ clusterDispersal, 
+             labeller = label_wrap_gen(multi_line=FALSE),
+             scale = "free", nrow = 3)+
+  labs(x = "Generation",
+       y = "Morphs (traits > 0.5%)")
+
+# save figures
+ggsave(plot = plot_morphs, filename = "figures/figure_morphs.png")
+
+# save data
+fwrite(data_morphs, file = "data/results/data_morphs.csv")
 
 #### check associations ####
 # load and add params to data
-data = lapply(file_list, function(l) {
+data_network = lapply(file_list, function(l) {
   load(l)
-  data_tmp = data_evolved_pop$pbsn
+  data_tmp = data_evolved_pop$network_measures
   params = data_evolved_pop[c("foodClusters", 
                               "clusterDispersal", "replicate")]
   data_tmp$foodClusters = params[["foodClusters"]]
@@ -79,17 +90,19 @@ data = lapply(file_list, function(l) {
 })
 
 # bind and check
-data = rbindlist(data)
-head(data)
+data_network = rbindlist(data_network)
+head(data_network)
+data_network = melt(data_network,
+                    id.vars = c("gen", "foodClusters", 
+                                "clusterDispersal", "replicate"))
 
 # plot and check
-ggplot(data)+
-  geom_bar(
-    aes(associations,
-        group = replicate
-    ),
-    alpha = 0.1,
-    position = position_identity()
+ggplot(data_network,
+       aes(x = gen,
+           group = interaction(variable, replicate)
+       ))+
+  geom_path(
+    aes(y = value
+    )
   )+
-  coord_cartesian(xlim = c(0, 10))+
-  facet_grid(foodClusters~clusterDispersal)
+  facet_wrap(~variable, scales = "free_y")
