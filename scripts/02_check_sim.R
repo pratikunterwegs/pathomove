3152309f048bf1e294a43cefeafc2b066d8a4e5d
Pratik Gupte <pratikgupte16@gmail.com>
2021-04-12 15:25:37 +0200

    minor edits

--- scripts/02_check_sim.R
+++ scripts/02_check_sim.R
@@ -1,4 +1,6 @@
 # check function
+devtools::build()
+devtools::install()
 
 library(snevo)
 library(ggplot2)
@@ -8,20 +10,20 @@ a = snevo::export_pop(101)
 
 # sim works
 a = snevo::do_simulation(
-  popsize = 100,
-  genmax = 1000, 
+  popsize = 50,
+  genmax = 25, 
   tmax = 100, 
-  nFood = 5000,
-  foodClusters = 1, 
+  nFood = 500,
+  foodClusters = 500, 
   clusterDispersal = 10,
-  landsize = 100
+  landsize = 50
 )
 
 b = a[["network_measures"]]
 
 ggplot(b,
   aes(
-      gen, interactions
+      gen, global_efficiency
     )
   )+
   geom_point()+
@@ -46,51 +48,21 @@ ggplot(d_summary)+
     trait_round, 
     fill = N
   ))+
-  scale_fill_distiller(
-    direction = 1
-  )
+  scale_fill_viridis_c()+
+  coord_cartesian(ylim = c(0, 1))
 
-sn = a[["network_measures"]]
+## check associations and trait
+d_sn = d[, list(
+  mean_a = mean(associations)
+), by = c("gen", "trait_round")]
 
-# run more sims
-
-landsize = 100
-popsize = 1000
-# make parameter file
-library(data.table)
-params = CJ(
-  genmax = 1000,
-  tmax = 100,
-  foodClusters = c(1, seq(25, 100, 25)),
-  clusterDispersal = 5,
-  replicate = seq(5)
-)
-
-data = Map(function(g, tm, cl, d) {
-  do_simulation(
-    popsize = popsize,
-    genmax = g,
-    tmax = tm,
-    nFood = 3000,
-    foodClusters = cl,
-    clusterDispersal = d,
-    landsize = landsize
-  )
-}, params$genmax, params$tmax, params$foodClusters, params$clusterDispersal)
-
-# get traits
-data2 = copy(params)
-data2$trait = lapply(data, `[[`, "p_ars")
-
-# plot
-library(ggplot2)
-
-# unlits
-data2 = data2[, unlist(trait, recursive = F),
-              by = names(params)]
-
-ggplot(data2)+
-  geom_histogram(aes(V1, fill = factor(replicate)))+
-  facet_grid(replicate ~ foodClusters,
-             labeller = label_both)+
-  xlim(0, 1)
+ggplot(d_sn)+
+  geom_tile(aes(
+    gen, 
+    trait_round, 
+    fill = mean_a
+  ))+
+  scale_fill_viridis_c(
+    trans = "sqrt"
+  )+
+  coord_cartesian(ylim = c(0, 1))
