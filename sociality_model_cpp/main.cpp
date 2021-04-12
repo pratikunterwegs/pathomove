f08583cb6dc2124948971354872cfb3ccb56b842
Pratik Gupte <pratikgupte16@gmail.com>
2021-04-12 15:25:22 +0200

    remove old args

--- sociality_model_cpp/main.cpp
+++ sociality_model_cpp/main.cpp
@@ -31,14 +31,7 @@ int main(int argc, char *argv[])
 {
     // process cliargs
     std::vector<std::string> cliArgs(argv, argv+argc);
-    // gather cli args
-//    const int genmax = std::stoi(cliArgs[1]);
-//    const int tmax = std::stoi(cliArgs[2]);
-//    const int clusters = std::stoi(cliArgs[3]);
-//    const double dispersal = std::stod(cliArgs[4]);
-//    const double landsize = std::stod(cliArgs[5]);
-    //        std::string rep = cliArgs[5];a
-//    do_simulation(genmax, tmax, clusters, dispersal, landsize);
-//    export_test_landscapes(clusters, dispersal, landsize, 3);
     test_wrapped_distance();
+
+    return 0;
 }
