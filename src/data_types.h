127e1e27d10a1cc34ae75aa723cf4f4013217059
Pratik Gupte <pratikgupte16@gmail.com>
2021-04-13 23:11:41 +0200

    new data type for move

--- src/data_types.h
+++ src/data_types.h
@@ -1,3 +1,5 @@
+#ifndef DATA_TYPES_H
+#define DATA_TYPES_H
 /// data types and related functions
 #include <vector>
 #include <random>
@@ -32,6 +34,56 @@ public:
     void updateNetworkData(Population &pop, const int gen, Network &pbsn);
     Rcpp::DataFrame getNetworkData();
 };
+
+struct moveData {
+public:
+    std::vector<std::vector<int> > id;
+    std::vector<std::vector<double> > trait;
+    std::vector<std::vector<double> > coordX;
+    std::vector<std::vector<double> > coordY;
+    std::vector<std::vector<double> > energy;
+    std::vector<int> timestep;
+
+    void updateMoveData (Population &pop, const int timestep);
+    Rcpp::List getMoveData ();
+};
+
+// function to update move
+void moveData::updateMoveData (Population &pop, const int timestep_) {
+    // get pop data
+    std::vector<int> idVec (pop.nAgents);
+    for (size_t i = 0; i < pop.nAgents; i++) {
+        idVec[i] = i;
+    }
+
+    id.push_back(idVec);
+    trait.push_back(pop.trait);
+    coordX.push_back(pop.coordX);
+    coordY.push_back(pop.coordY);
+    energy.push_back(pop.energy);
+    timestep.push_back(timestep_);
+}
+
+// function to return gen data as an rcpp list
+Rcpp::List moveData::getMoveData() {
+    Rcpp::List moveDataList (timestep.size());
+    for (size_t i = 0; i < timestep.size(); i++)
+    {
+        moveDataList[i] = DataFrame::create(
+            Named("id") = id[i],
+            Named("energy") = energy[i],
+            Named("x") = coordX[i],
+            Named("Y") = coordY[i],
+            Named("trait") = trait[i]
+        );
+    }
+    List dataToReturn = List::create(
+        Named("move_data") = moveDataList,
+        Named("timestep") = timestep
+    );
+
+    return dataToReturn;
+}
 
 // function to update gendata
 void genData::updateGenData (Population &pop, const int gen_) {
@@ -84,3 +136,5 @@ Rcpp::DataFrame networkData::getNetworkData() {
 
     return networkData;
 }
+
+#endif  //
