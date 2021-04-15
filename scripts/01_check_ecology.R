44ff5b5de69faa73f5805dafad537f73a6dd002e
Pratik Gupte <pratikgupte16@gmail.com>
2021-04-15 16:21:54 +0200

    params

--- scripts/01_check_ecology.R
+++ scripts/01_check_ecology.R
@@ -11,15 +11,15 @@ library(data.table)
 a = snevo::do_eco_sim(
     popsize = 100,
     landsize = 10,
-    nFood = 500,
-    nClusters = 100,
+    nFood = 100,
+    nClusters = 20,
     clusterDispersal = 0.1,
-    maxAct = 0.99,
-    activityRatio = 0.2,
+    maxAct = 1,
+    activityRatio = 0.1,
     pInactive = 0.8,
     collective = FALSE,
     sensoryRange = 1,
-    tmax = 25,
+    tmax = 10,
     scenes = 10
 )
 
@@ -32,11 +32,19 @@ d = Map(function(df, sc) {
 d = rbindlist(d)
 hist(d$trait)
 
-ggplot(d)+
-geom_boxplot(
+d_summary = d[,list(
+    mean_assoc = mean(associations),
+    sd_assoc = sd(associations)
+), by = c("scene", "trait")]
+
+ggplot(d_summary)+
+geom_pointrange(
     aes(
-        factor(trait), associations,
-        fill = factor(trait)
-    )
-)+
+        scene, mean_assoc,
+        ymin = mean_assoc - sd_assoc,
+        ymax = mean_assoc + sd_assoc,
+        colour = factor(trait)
+    ),
+    position = position_dodge(width = 0.2)
+)
 facet_grid(~scene)
