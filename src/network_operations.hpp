d651948e1dda401a3f569468f3958f94f5cbfb7c
Pratik Gupte <pratikgupte16@gmail.com>
2021-04-15 16:21:31 +0200

    calc degree

--- src/network_operations.hpp
+++ src/network_operations.hpp
@@ -8,6 +8,7 @@
 Rcpp::Environment igraph("package:igraph");
 Rcpp::Function graph_from_df = igraph["graph.data.frame"];
 Rcpp::Function graph_diameter = igraph["diameter"];
+Rcpp::Function graph_degree = igraph["degree"];
 
 /// function to make igraph network from pbsn
 std::vector<double> networkMeasures(Network &pbsn, Population &pop) {
@@ -24,6 +25,19 @@ std::vector<double> networkMeasures(Network &pbsn, Population &pop) {
         vDiam[0], // diameter
         vDiam[0] * (pop.nAgents / static_cast<double>(pbsn_data.nrows())) // global efficiency
     };
+}
+
+std::vector<int> getDegree(Network &pbsn, Population &pop) {
+    Rcpp::DataFrame pbsn_data = returnPbsn(pop, pbsn);
+    List graph = graph_from_df(pbsn_data, Named("directed")=false);
+
+    SEXP degree = graph_degree(graph);
+
+    std::vector<int> vDeg = Rcpp::as< std::vector<int> >(degree);
+
+    Rcout << "vedg size = " << vDeg.size() << "\n";
+
+    return vDeg;
 }
 
 #endif // NETWORK_OPERATIONS_HPP
