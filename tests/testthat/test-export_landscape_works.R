424412a0e62c47e8de566132925ed2e566b7fb42
Pratik Gupte <pratikgupte16@gmail.com>
2021-06-05 08:34:38 +0200

    skip tests

--- tests/testthat/test-export_landscape_works.R
+++ tests/testthat/test-export_landscape_works.R
@@ -1,7 +1,7 @@
 test_that("export landscape works", {
-  skip()
-  # check function runes
-  testthat::expect_silent(
-    export_test_landscapes(100, 1, 100, 1, 1)
-  )
+  skip("skipped")
+  # # check function runes
+  # testthat::expect_silent(
+  #   export_test_landscapes(100, 1, 100, 1, 1)
+  # )
 })
--- tests/testthat/test-sim_works_basic.R
+++ tests/testthat/test-sim_works_basic.R
@@ -1,37 +1,37 @@
 test_that("simulation works", {
-  skip()
-  # parameters
-  popsize <- 100
-
-  # check function runes
-  data_evolved_pop <- snevo::do_simulation(
-    nFood = 300,
-    popsize = popsize,
-    genmax = 10,
-    tmax = 10,
-    foodClusters = 1,
-    clusterDispersal = 0.1,
-    landsize = 100
-  )
-
-  # check is list
-  testthat::expect_is(
-    data_evolved_pop, "list"
-  )
-
-  # check length is two
-  testthat::expect_identical(
-    length(data_evolved_pop), 2L
-  )
-
-  # check elements are data frames
-  invisible(
-    lapply(data_evolved_pop$trait_data$pop_data, 
-           testthat::expect_is, class = "data.frame")
-  )
-
-  # check trait data has popsize rows, checking only one df for now
-  testthat::expect_identical(
-    nrow(data_evolved_pop[["trait_data"]]$pop_data[[1]]), as.integer(popsize)
-  )
+  skip("skipped")
+  # # parameters
+  # popsize <- 100
+  # 
+  # # check function runes
+  # data_evolved_pop <- snevo::do_simulation(
+  #   nFood = 300,
+  #   popsize = popsize,
+  #   genmax = 10,
+  #   tmax = 10,
+  #   foodClusters = 1,
+  #   clusterDispersal = 0.1,
+  #   landsize = 100
+  # )
+  # 
+  # # check is list
+  # testthat::expect_is(
+  #   data_evolved_pop, "list"
+  # )
+  # 
+  # # check length is two
+  # testthat::expect_identical(
+  #   length(data_evolved_pop), 2L
+  # )
+  # 
+  # # check elements are data frames
+  # invisible(
+  #   lapply(data_evolved_pop$trait_data$pop_data, 
+  #          testthat::expect_is, class = "data.frame")
+  # )
+  # 
+  # # check trait data has popsize rows, checking only one df for now
+  # testthat::expect_identical(
+  #   nrow(data_evolved_pop[["trait_data"]]$pop_data[[1]]), as.integer(popsize)
+  # )
 })
