d3603b4086818dfcecbc98ff49f203333b1f53bc
Pratik Gupte <pratikgupte16@gmail.com>
2021-06-05 08:31:48 +0200

    move defs and remove headers

--- src/data_types.h
+++ src/data_types.h
@@ -6,11 +6,8 @@
 #include <iostream>
 #include <fstream>
 #include <algorithm>
-#include "parameters.h"
-#include "landscape.h"
 #include "agents.h"
 #include "network.h"
-#include "network_operations.hpp"
 
 // define a struct holding a vector of data frames which holds generation wise data
 struct genData {
@@ -25,17 +22,6 @@ public:
     Rcpp::List getGenData ();
 };
 
-// struct networkData {
-// public:
-//     std::vector<int> interactions;
-//     std::vector<double> diameter;
-//     std::vector<double> global_efficiency;
-//     std::vector<int> gens;
-
-//     void updateNetworkData(Population &pop, const int gen, Network &pbsn);
-//     Rcpp::DataFrame getNetworkData();
-// };
-
 struct moveData {
 public:
     std::vector<std::vector<int> > id;
@@ -47,99 +33,5 @@ public:
     void updateMoveData (Population &pop, const int timestep);
     Rcpp::List getMoveData ();
 };
-
-// function to update move
-void moveData::updateMoveData (Population &pop, const int timestep_) {
-    // get pop data
-    std::vector<int> idVec (pop.nAgents);
-    for (size_t i = 0; i < pop.nAgents; i++) {
-        idVec[i] = i;
-    }
-
-    id.push_back(idVec);
-    coordX.push_back(pop.coordX);
-    coordY.push_back(pop.coordY);
-    energy.push_back(pop.energy);
-    timestep.push_back(timestep_);
-}
-
-// function to return move data as an rcpp list
-Rcpp::List moveData::getMoveData() {
-    Rcpp::List moveDataList (timestep.size());
-    for (size_t i = 0; i < timestep.size(); i++)
-    {
-        moveDataList[i] = DataFrame::create(
-            Named("id") = id[i],
-            Named("energy") = energy[i],
-            Named("x") = coordX[i],
-            Named("Y") = coordY[i]
-        );
-    }
-    List dataToReturn = List::create(
-        Named("move_data") = moveDataList,
-        Named("timestep") = timestep
-    );
-
-    return dataToReturn;
-}
-
-// function to update gendata
-void genData::updateGenData (Population &pop, const int gen_) {
-    // get pop data
-    genEnergyVec.push_back(pop.energy);
-    traitMatrix.push_back(pop.traitMatrix);
-    // genAssocVec.push_back(pop.associations);
-    // genDegreeVec.push_back(pop.degree);
-    gens.push_back(gen_);
-}
-
-// void networkData::updateNetworkData(Population &pop, const int gen, Network &pbsn) {
-
-//     std::vector<double> tmpNetworkData = networkMeasures(pbsn, pop);
-
-//     assert (tmpNetworkData.size() == 3 && "wrong size network measures");
-
-//     interactions.push_back(tmpNetworkData[0]);
-//     diameter.push_back(tmpNetworkData[1]);
-//     global_efficiency.push_back(tmpNetworkData[2]);
-//     gens.push_back(gen);
-// }
-
-// function to return gen data as an rcpp list
-Rcpp::List genData::getGenData() {
-    Rcpp::List genDataList (gens.size());
-    for (size_t i = 0; i < gens.size(); i++)
-    {
-        genDataList[i] = DataFrame::create(
-            Named("energy") = genEnergyVec[i],
-            Named("trait1") = traitMatrix[i][0],
-            Named("trait2") = traitMatrix[i][1],
-            Named("trait3") = traitMatrix[i][2],
-            Named("trait4") = traitMatrix[i][3],
-            Named("trait5") = traitMatrix[i][4],
-            Named("trait6") = traitMatrix[i][5]
-            // Named("associations") = genAssocVec[i],
-            // Named("degree") = genDegreeVec[i]
-        );
-    }
-    List dataToReturn = List::create(
-        Named("pop_data") = genDataList,
-        Named("gens") = gens
-    );
-
-    return dataToReturn;
-}
-
-// function to return network data as a DataFrame
-// Rcpp::DataFrame networkData::getNetworkData() {
-//     Rcpp::DataFrame networkData = Rcpp::DataFrame::create(
-//                 Named("gen") = gens,
-//                 Named("interactions") = interactions,
-//                 Named("diameter") = diameter,
-//                 Named("global_efficiency") = global_efficiency
-//             );
-
-//     return networkData;
-// }
 
 #endif  //
