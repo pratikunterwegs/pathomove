85c4e9b23dbbcfeb92fec6d917b6bd18fb9d8ba9
Pratik Gupte <pratikgupte16@gmail.com>
2021-06-22 12:12:45 +0200

    more flexible params maker

--- R/fun_make_parameters.R
+++ R/fun_make_parameters.R
@@ -1,48 +1,21 @@
 #' Write parameters as CSV.
 #'
-#' @param popsize Max population size.
-#' @param tmax Timesteps.
 #' @param replicates How many replicates.
-#' @param which_file Which file to write to.
-#' @param genmax Max generation.
-#' @param foodClusters How many clusters.
-#' @param clusterDispersal How spread out food is.
-#' @param nFood N food items, integer.
-#' @param landsize Landscape size, double value.
-#' @param collective Whether movement is collective-aware.
-#' @param competitionCost The cost of competition.
-#' @param nScenes How many scenes.
+#' @param which_file Where to save these parameters.
+#' @param ... Pass arguments to \code{run\_noisewalker}.
 #'
 #' @return Writes a parameter file.
 #' @export
 #'
 make_parameter_file <- function(
-  genmax = 1000,
-  popsize = 1000,
-  tmax = 100,
-  nFood = 3000,
-  landsize = 300,
-  foodClusters = seq(1, 10),
-  clusterDispersal = seq(1, 5),
-  collective = FALSE,
-  competitionCost = 0.1,
-  nScenes = 10,
-  replicates = 10,
+  replicates,
+  ...,
   which_file = "this_file.csv"
 ) {
 
   # make combinations
   parameters <- data.table::CJ(
-    nFood,
-    landsize,
-    genmax,
-    popsize,
-    tmax,
-    foodClusters,
-    clusterDispersal,
-    collective,
-    competitionCost,
-    nScenes,
+    ...,
     replicate = seq(replicates)
   )
 
