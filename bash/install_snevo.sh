54fb86f266ea29141d654b52007473c8bb7c486d
Pratik Gupte <pratikgupte16@gmail.com>
2021-06-22 12:12:26 +0200

    sink to log

--- bash/install_snevo.sh
+++ bash/install_snevo.sh
@@ -3,4 +3,4 @@
 ml load R/4.0.0-foss-2020a
 ml load Boost/1.72.0-foss-2019a
 Rscript --slave -e 'devtools::build()'
-Rscript --slave -e 'devtools::install()'
+Rscript --slave -e 'sink("install_log.log"); devtools::install(); sink()'
