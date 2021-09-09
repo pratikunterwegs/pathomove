19bed07595414284ea1ec53bd13fb7638961bd3e
Pratik Gupte <pratikgupte16@gmail.com>
2021-09-09 20:21:35 +0200

    def decl run pathomove

--- src/simulations.hpp
+++ src/simulations.hpp
@@ -41,40 +41,15 @@ public:
 
 };
 
-//' Runs the pathomove simulation.
-//'
-//' @description Run the simulation using parameters passed as
-//' arguments to the corresponding R function.
-//'
-//' @param popsize The population size.
-//' @param genmax The maximum number of generations per simulation.
-//' @param tmax The number of timesteps per generation.
-//' @param nFood The number of food items.
-//' @param foodClusters Number of clusters around which food is generated.
-//' @param clusterDispersal How dispersed food is around the cluster centre.
-//' @param landsize The size of the landscape as a numeric (double).
-//' @param competitionCost Cost of associations.
-//' @param sensoryRange The sensory range.
-//' @param nScenes How many scenes.
-//' @param stopTime The handling time.
-//' @return A data frame of the evolved population traits.
-// [[Rcpp::export]]
-/*Rcpp::List*/ int run_pathomove(const int popsize, const int scenario,
-                                  const int nItems, const float landsize,
-                                  const int nClusters,
-                                  const float clusterSpread,
-                                  const int tmax,
-                                  const int genmax,
-                                  const float range_food,
-                                  const float range_agents,
-                                  const int handling_time,
-                                  const int regen_time) {
-    simulation this_sim(popsize, scenario, nItems, landsize,
-                        nClusters, clusterSpread, tmax, genmax,
-                        range_food, range_agents,
-                        handling_time, regen_time);
-
-    return 0;
-}
+Rcpp::List run_pathomove(const int popsize, const int scenario,
+                        const int nItems, const float landsize,
+                        const int nClusters,
+                        const float clusterSpread,
+                        const int tmax,
+                        const int genmax,
+                        const float range_food,
+                        const float range_agents,
+                        const int handling_time,
+                        const int regen_time);
 
 #endif // SIMULATIONS_H
