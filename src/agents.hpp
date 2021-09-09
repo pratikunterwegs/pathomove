74417377ce557f15e239a5a3b6b5be149aaad4e6
Pratik Gupte <pratikgupte16@gmail.com>
2021-09-09 20:11:20 +0200

    update decl resources class

--- src/agents.hpp
+++ src/agents.hpp
@@ -13,23 +13,35 @@
 // Agent class
 struct Population {
 public:
-    Population(const int popsize, const double beginTrait) :
+    Population(const int popsize, const float range_agents,
+    const float range_food, const int handling_time,
+    const int nInfected,
+    const float pTransmit) :
+        // agents and traits
         nAgents (popsize),
         coordX (popsize, 0.0),
         coordY (popsize, 0.0),
         energy (popsize, 0.001),
         coef_nbrs (popsize, 1.f),
         coef_food (popsize, 1.f),
+        
         // count stationary behaviour
         counter (popsize, 0),
         // associations
         associations(popsize, 0),
-        degree(popsize, 0)
+        degree(popsize, 0),
+        range_agents(range_agents),
+        range_food(range_food),
+        handling_time(handling_time),
+        order(popsize, 1),
+        infected(popsize, false),
+        nInfected (nInfected),
+        pTransmit (pTransmit)
 
     {}
     ~Population() {}
 
-    int nAgents = 0;
+    const int nAgents;
     std::vector<float> coordX;
     std::vector<float> coordY;
     std::vector<float> energy;
@@ -38,26 +50,31 @@ public:
     std::vector<int> counter;
     std::vector<int> associations; // number of total interactions
     std::vector<int> degree;
+    const float range_agents, range_food;
+    const int handling_time;
+    std::vector<int> order;
+    std::vector<bool> infected;
+    const int nInfected;
+    const float pTransmit;
 
     // position rtree
     bgi::rtree< value, bgi::quadratic<16> > agentRtree;
 
     // funs for pop
-    void initPop (int popsize);
+    void shufflePop();
     void setTrait ();
     void initPos(Resources food);
 
-    void competitionCosts(const float competitionCost);
     void updateRtree();
     std::pair<int, std::vector<int> > countNearby (
         bgi::rtree< value, bgi::quadratic<16> > treeToQuery,
-        size_t id, float sensoryRange,
+        size_t id, float sRange,
         const float xloc, const float yloc);
 
-    void move(size_t id, Resources food, const float moveCost, float sensoryRange);
-    void forage(size_t individual, Resources &food, float distance, const int stopTime);
+    void move(Resources food);
+    void forage(Resources &food);
     
-    std::vector<float> normaliseIntake();
+    std::vector<float> handleFitness();
     void Reproduce();
     
     // for network
