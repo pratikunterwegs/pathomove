d386bab40b6b654c30958744ec876521adbdd684
Pratik Gupte <pratikgupte16@gmail.com>
2021-09-09 20:13:53 +0200

    update sim ef

--- src/simulations.cpp
+++ src/simulations.cpp
@@ -9,30 +9,28 @@
 
 using namespace Rcpp;
 
-// function to evolve population
-void evolve_pop(int genmax, double tmax,
-                Population &pop,
-                Resources &food,
-                genData &thisGenData,
-                double competitionCost,
-                float sensoryRange,
-                const int scenes,
-                const int stopTime)
-{
+Rcpp::List simulation::do_simulation() {
+
+    // prepare landscape and pop
+    food.initResources();
+    food.countAvailable();
+    Rcpp::Rcout << "landscape with " << food.nClusters << " clusters\n";
+
+    pop.setTrait();
+    Rcpp::Rcout << "pop with" << pop.nAgents << " agents for " << genmax << " gens " << tmax << " timesteps\n";
+
+    // prepare social network struct
+    // Network pbsn;
+    // pbsn.initAssociations(pop.nAgents);
 
     // set seed
     unsigned seed = static_cast<unsigned> (std::chrono::system_clock::now().time_since_epoch().count());
     rng.seed(seed);
 
-    std::vector<int> shuffleVec (pop.nAgents, 0);
-    for (int i = 0; i < pop.nAgents; ++i) {
-        shuffleVec[i] = i;
-    }
-
     // go over gens
     for(int gen = 0; gen < genmax; gen++) {
         // go over scenes
-        for(int s = 0; s < scenes; s++) {
+
             food.initResources();
             food.countAvailable();
             // reset population associations and degree
@@ -47,29 +45,21 @@ void evolve_pop(int genmax, double tmax,
             // pbsn.initAssociations(pop.nAgents);
             // pbsn.initAdjMat(pop.nAgents);
 
-            // no to gillespie loop
-            for (size_t t = 0; t < tmax; t++)
+            // timesteps start here
+            for (size_t t = 0; t < static_cast<size_t>(tmax); t++)
             {
-                pop.updateRtree();
-                // movement section
-                for (size_t i = 0; i < pop.nAgents; i++)
-                {
-                    size_t id_to_move = shuffleVec[i];
-                    // check if agent can move
-                    if (pop.counter[id_to_move] <= 0) {
-                        pop.move(id_to_move, food, competitionCost, sensoryRange); // movecost hardcoded to 0
-                    } else if (pop.counter[id_to_move] > 0) {
-                        pop.counter[id_to_move] --;
-                    }
-                }
+                // resources regrow
+                food.regenerate();
 
-                // foraging section
-                for (int i = 0; i < pop.nAgents; i++) {
-                    size_t id_to_move = shuffleVec[i];
-                    pop.forage(id_to_move, food, sensoryRange, stopTime);
-                    // count associations
-                    pop.associations[id_to_move] += (pop.countNearby(pop.agentRtree, id_to_move, sensoryRange, pop.coordX[i], pop.coordY[i])).first;
-                }
+                pop.updateRtree();
+
+                // movement section
+                pop.move(food);
+
+                // foraging
+                pop.forage(food);
+
+                // associations not upated
 
                 // PBSN etc
                 // pop.updatePbsn(pbsn, sensoryRange);
@@ -78,13 +68,12 @@ void evolve_pop(int genmax, double tmax,
             // pop.degree = getDegree(pbsn);
 
             // food.countAvailable();
-        }
         // generation ends here
         // update gendata
         if (gen == (genmax - 1)) {
-            thisGenData.updateGenData(pop, gen);
+            gen_data.updateGenData(pop, gen);
         }
-    
+
         // thisNetworkData.updateNetworkData(pop, gen, pbsn);
         // subtract competition costs
         // pop.competitionCosts(competitionCost);
@@ -93,54 +82,7 @@ void evolve_pop(int genmax, double tmax,
     }
     // all gens end here
 
-}
-
-//' Runs the sociality model simulation.
-//'
-//' @description Run the simulation using parameters passed as
-//' arguments to the corresponding R function.
-//' 
-//' @param popsize The population size.
-//' @param genmax The maximum number of generations per simulation.
-//' @param tmax The number of timesteps per generation.
-//' @param nFood The number of food items.
-//' @param foodClusters Number of clusters around which food is generated.
-//' @param clusterDispersal How dispersed food is around the cluster centre.
-//' @param landsize The size of the landscape as a numeric (double).
-//' @param competitionCost Cost of associations.
-//' @param sensoryRange The sensory range.
-//' @param nScenes How many scenes.
-//' @param stopTime The handling time.
-//' @return A data frame of the evolved population traits.
-// [[Rcpp::export]]
-Rcpp::List do_simulation(int popsize, int genmax, int tmax, 
-                         int nFood, int foodClusters, double clusterDispersal, double landsize,
-                         double competitionCost, float sensoryRange,
-                         const int nScenes, const int stopTime) {
-
-    // prepare landscape
-    Resources food (nFood, landsize, foodClusters, clusterDispersal);
-    food.initResources();
-    food.countAvailable();
-    Rcpp::Rcout << "landscape with " << foodClusters << " clusters\n";
-    /// export landscape
-
-    // prepare population
-    Population pop (popsize, 0);
-    // pop.initPop(popsize);
-    pop.setTrait();
-    Rcpp::Rcout << pop.nAgents << " agents over " << genmax << " gens of " << tmax << " timesteps\n";
-
-    // prepare data structure
-    genData thisGenData;
-    // prepare social network struct
-    // Network pbsn;
-    // pbsn.initAssociations(pop.nAgents);
-
-    // evolve population and store data
-    evolve_pop(genmax, tmax, pop, food, thisGenData, competitionCost, sensoryRange, nScenes, stopTime);
-
     Rcpp::Rcout << "data prepared\n";
 
-    return thisGenData.getGenData();
+    return gen_data.getGenData();
 }
