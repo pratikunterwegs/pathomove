ef9b9ee24a9346d9f2b8b56b337b7157851bb939
Pratik Gupte <pratikgupte16@gmail.com>
2021-09-17 15:45:07 +0200

    R script to check model

--- scripts/01_check_ecology.R
+++ scripts/01_check_ecology.R
@@ -9,20 +9,29 @@ library(snevo)
 library(ggplot2)
 library(data.table)
 
+l = snevo::get_test_landscape(
+  nItems = 2000,
+  landsize = 25,
+  nClusters = 100, 
+  clusterSpread = 0.15
+)
+plot(l)
+
 a = run_pathomove(
   scenario = 1,
-  popsize = 500,
-  nItems = 1000,
-  landsize = 10,
-  nClusters = 8,
-  clusterSpread = 2,
-  tmax = 50,
-  genmax = 1000,
+  popsize = 200,
+  nItems = 500,
+  landsize = 25,
+  nClusters = 100,
+  clusterSpread = 0.1,
+  tmax = 200,
+  genmax = 500,
   range_food = 0.5,
   range_agents = 1,
-  handling_time = 3,
+  handling_time = 5,
   regen_time = 10
 )
+
 names(a)
 
 #### handle data ####
@@ -34,6 +43,13 @@ b = Map(function(l, g) {
 }, b$pop_data, b$gens)
 b = rbindlist(b)
 b
+# b = b[(gen %% 100 == 0) | (gen == 9999),] 
+
+ggplot(b)+
+  geom_bin2d(
+    aes(gen, energy),
+    binwidth = c(10, 1)
+  )
 
 #### plot data ####
 b = melt(b, id.vars = c("gen", "id"))
@@ -41,13 +57,53 @@ b = melt(b, id.vars = c("gen", "id"))
 # energy = b[variable == "energy",]
 wts = b[variable != "energy",]
 
+wts[, value := tanh(value * 20)]
+
 ggplot(wts)+
   geom_bin2d(
     aes(gen, value),
-    binwidth = c(1, 0.005)
+    binwidth = c(10, 0.02),
+    show.legend = F
   )+
   scale_y_continuous(
-    trans = ggallin::pseudolog10_trans
+    # limits = c(-0.3, 0.3),
+    # limits = c(-0.5, 0.5)
+    # trans = ggallin::ssqrt_trans
   )+
-  scale_fill_viridis_c()+
-  facet_grid(~variable)
+  scale_fill_viridis_c(
+    option = "C", direction = -1,
+    begin = 0.2, end = 1
+  )+
+  theme_minimal()+
+  facet_wrap(~variable, ncol = 2)
+
+# scale weights
+wts_wide = copy(b[variable != "energy"])
+wts_wide[, scaled_val := value / sum(abs(value)),
+         by = c("gen", "id")]
+wts_wide = dcast(
+  wts_wide[, !("value")], 
+  gen + id ~ variable, 
+  value.var = "scaled_val"
+)
+
+ggplot(wts_wide[gen %% 100 == 0 | gen == max(gen)])+
+  geom_jitter(
+    aes(coef_nbrs, coef_food2,
+        fill = coef_nbrs2),
+    shape = 21,
+    size = 2
+  )+
+  scale_fill_viridis_c(
+    option = 'H',
+    limits = c(-1, 1), 
+    direction = 1
+  )+
+  scale_x_continuous(
+    trans = ggallin::ssqrt_trans
+  )+
+  scale_y_continuous(
+    trans = ggallin::ssqrt_trans
+  )+
+  theme_minimal()+
+  facet_wrap(~ gen)
