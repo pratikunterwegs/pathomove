02ccce9b0d49fd20bc81f3e31941038831bd03df
Pratik Gupte <pratikgupte16@gmail.com>
2021-09-23 12:38:52 +0200

    namespacing rcpp

--- src/network.cpp
+++ src/network.cpp
@@ -1,31 +1,42 @@
 /// functions for network operations
 
 #include "network.hpp"
+#include <Rcpp.h>
+
+/// including R environment with the igraph package
+Rcpp::Environment igraph("package:igraph");
+Rcpp::Function graph_diameter = igraph["diameter"];
+Rcpp::Function graph_degree = igraph["degree"];
+Rcpp::Function graph_from_adjmat = igraph["graph.adjacency"];
+Rcpp::Function df_from_graph = igraph["as_data_frame"];
+Rcpp::Function network_size = igraph["gsize"];
 
 /// make graph from adjacency matrix
 void Network::adjMat_to_graph() {
-    graph = graph_from_adjmat(adjMat, Named("diag")=false, Named("weighted")=true);
+    graph = graph_from_adjmat(adjMat, Rcpp::Named("diag")=false, Rcpp::Named("weighted")=true);
 }
 
 /// function to get metrics from adjacency matrix
-std::vector<double> Network::ntwkMeasures() {
+std::vector<float> Network::ntwkMeasures() {
 
-    SEXP diameter = graph_diameter(graph, Named("directed")=false);
+    SEXP diameter = graph_diameter(graph, Rcpp::Named("directed")=false);
     SEXP size = network_size(graph);
 
     Rcpp::NumericVector vDiam (diameter);
     Rcpp::NumericVector vSize (size);
 
-    return std::vector<double> { 
+    std::vector<float> measures { 
         vDiam[0], // diameter
-        vDiam[0] * (pop.nAgents / vSize[0]) // global efficiency
+        vDiam[0] * (nVertices / vSize[0]) // global efficiency
     };
+
+    return measures;
 }
 
 /// get individual degree from data frame
 std::vector<int> Network::getDegree() {
     
-    SEXP degree = graph_degree(graph, Named("loops")=false);
+    SEXP degree = graph_degree(graph, Rcpp::Named("loops")=false);
 
     std::vector<int> vDeg = Rcpp::as< std::vector<int> >(degree);
 
@@ -33,7 +44,7 @@ std::vector<int> Network::getDegree() {
 }
 
 /// get graph as dataframe
-Rcpp::DataFrame returnDF(){
+Rcpp::DataFrame Network::returnDF(){
     
-    return Rcpp::DataFrame( returnDF(graph) );
+    return Rcpp::DataFrame( df_from_graph(graph) );
 }
--- src/network.hpp
+++ src/network.hpp
@@ -1,37 +1,30 @@
-#ifndef NETWORK_H
-#define NETWORK_H
+#pragma once
 
 #include <vector>
 #include <cassert>
 #include <Rcpp.h>
-
-/// including R environment with the igraph package
-Rcpp::Environment igraph("package:igraph");
-Rcpp::Function graph_diameter = igraph["diameter"];
-Rcpp::Function graph_degree = igraph["degree"];
-Rcpp::Function graph_from_adjmat = igraph["graph.adjacency"];
-Rcpp::Function df_from_graph = igraph["as_data_frame"]
-Rcpp::Function network_size = igraph["gsize"]
 
 /// the network structure, which holds an adjacency matrix
 // network should be a member of population later
 // network has funs to return network metrics and the adj matrix
 struct Network {
 public:
-    Network(const int popsize):
+    Network(const int popsize) :
+        nVertices(popsize),
         adjMat (popsize, popsize),
-        graph
-        
+        graph ()
     {}
     ~Network() {}
+
+    // members
+    const int nVertices;
     Rcpp::NumericMatrix adjMat;
     Rcpp::List graph;
 
     // functions
     void adjMat_to_graph();
-    std::vector<double> ntwkMeasures();
+    std::vector<float> ntwkMeasures();
     std::vector<int> getDegree();
     Rcpp::DataFrame returnDF();
 };
 
-#endif // NETWORK_H
