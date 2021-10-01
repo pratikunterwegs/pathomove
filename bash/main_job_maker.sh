38d17ed9b94690426cfa52f7568fc8f24193a823
Pratik Gupte <pratikgupte16@gmail.com>
2021-10-01 21:23:08 +0200

    allow threads

--- bash/main_job_maker.sh
+++ bash/main_job_maker.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#SBATCH --time=03:00:00
+#SBATCH --time=00:10:00
 #SBATCH --partition=regular
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --job-name=snevo_simulation
+#SBATCH --cpus-per-task=4
 #SBATCH --array=1-n_array
 #SBATCH --output=/home/p284074/snevo/data/output/snevo_output_%a.log
-#SBATCH --mem=5000
+#SBATCH --mem=2000
 
 pwd
 ml load R/4.0.0-foss-2020a
