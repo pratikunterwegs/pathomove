44dbfd38da80a42e9b2a9f636f0fadba70cd8fa4
Pratik Gupte <pratikgupte16@gmail.com>
2021-10-01 11:24:29 +0200

    do not load tbb

--- bash/install_snevo.sh
+++ bash/install_snevo.sh
@@ -2,6 +2,5 @@
 
 ml load R/4.0.0-foss-2020a
 ml load Boost/1.72.0-foss-2019a
-ml load tbb/2020.3-GCCcore-10.2.0
 Rscript --slave -e 'devtools::build()'
 Rscript --slave -e 'sink("install_log.log"); devtools::install(); sink()'
