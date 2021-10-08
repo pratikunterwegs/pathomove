441cb0c58caae3b87aff2c5443a0b06f14707743
Pratik Gupte <pratikgupte16@gmail.com>
2021-10-08 18:07:22 +0200

    vis networks

--- scripts/check_pkg_install.R
+++ scripts/check_pkg_install.R
@@ -12,42 +12,43 @@ library(ggplot2)
 library(data.table)
 
 l = snevo::get_test_landscape(
-  nItems = 1000,
+  nItems = 500,
   landsize = 200,
-  nClusters = 50, 
-  clusterSpread = 5,
-  regen_time = 10
+  nClusters = 100, 
+  clusterSpread = 10,
+  regen_time = 50
 )
 ggplot(l)+
   geom_point(
-    aes(x, y, col = tAvail),
-    size = 0.3
+    aes(x, y, col = tAvail)
+    # size = 0.3
   )+
   scale_colour_viridis_b(
     option = "H",
     direction = 1,
     breaks = c(0, 1, 2, 5, 10)
-  )
+  )+
+  coord_equal()
 
 {t1 = Sys.time()
 invisible(
   x = {
     a = snevo::run_pathomove(
-      scenario = 0,
+      scenario = 1,
       popsize = 500,
-      nItems = 1000,
+      nItems = 500,
       landsize = 200,
       nClusters = 50,
-      clusterSpread = 5,
+      clusterSpread = 4,
       tmax = 100,
       genmax = 500,
       range_food = 1,
       range_agents = 1,
       handling_time = 5,
-      regen_time = 200,
+      regen_time = 50,
       pTransmit = 0.01,
       initialInfections = 2,
-      costInfect = 0.00,
+      costInfect = 0.02,
       nThreads = 2
     )
   }
@@ -74,12 +75,6 @@ b = rbindlist(b)
 b
 # b = b[(gen %% 100 == 0) | (gen == 9999),] 
 
-ggplot(b)+
-  geom_bin2d(
-    aes(gen, energy),
-    binwidth = c(2, 1)
-  )
-
 #### plot data ####
 b = melt(b, id.vars = c("gen", "id"))
 
@@ -89,15 +84,17 @@ wts = b[!variable %in% c("energy", "assoc", "t_infec", "moved", "degree"),]
 wts[, value := tanh(value * 20)]
 
 ggplot(wts)+
+  geom_hline(
+    yintercept = 0, lty = 2,
+    col = "grey", size = 0.2
+  )+
   geom_bin2d(
     aes(gen, value),
     binwidth = c(2, 0.02),
     show.legend = F
   )+
   scale_y_continuous(
-    # limits = c(-0.3, 0.3),
-    # limits = c(-0.5, 0.5)
-    # trans = ggallin::ssqrt_trans
+    trans = ggallin::ssqrt_trans
   )+
   scale_fill_viridis_c(
     option = "A", direction = -1,
@@ -105,19 +102,6 @@ ggplot(wts)+
   )+
   theme_test()+
   facet_wrap(~variable, ncol = 2)
-
-# plot associations per gen
-ggplot(b[variable == "assoc"])+
-  geom_bin2d(
-    aes(gen, value),
-    binwidth = c(2, 500)
-  )+
-  scale_fill_viridis_c(
-    option = "H", direction = -1,
-    limits = c(3, NA),
-    na.value = "transparent",
-    trans = "sqrt"
-  )
 
 # scale weights
 wts_wide = copy(b[!variable %in% c("energy", "assoc", "t_infec", "moved", "degree"),])
@@ -140,13 +124,13 @@ ggplot(wts_wide[gen %% 100 == 0 | gen == max(gen)])+
     aes(sF, sH,
         fill = sN),
     shape = 21,
-    colour = 'black',
+    # colour = "transparent",
     stroke = 0.1,
-    size = 2,
-    alpha = 0.5
+    size = 4,
+    alpha = 0.8
   )+
-  colorspace::scale_fill_continuous_diverging(
-    palette = "Berlin", rev = T,
+  colorspace::scale_fill_continuous_sequential(
+    palette = "Batlow", rev = T,
     limits = c(-1, 1),
     trans = ggallin::ssqrt_trans
   )+
@@ -156,14 +140,16 @@ ggplot(wts_wide[gen %% 100 == 0 | gen == max(gen)])+
   scale_y_continuous(
     # trans = ggallin::ssqrt_trans
   )+
-  theme_bw()+
-  facet_wrap(~ gen)+
-  coord_cartesian(
+  facet_wrap(~ gen, labeller = label_both)+
+  coord_equal(
     xlim = c(-1, 1),
     ylim = c(-1, 1)
   )
 
 #### explore network ####
 library(igraph)
-g = data[[2]]
-g = igraph::graph_from_adjacency_matrix(g, mode = "upper", weighted = TRUE)
+g = data[["matrices"]][4:6]
+g = g[[3]]
+g = igraph::graph_from_adjacency_matrix(g, mode = "upper", weighted = TRUE, diag = F)
+
+plot(g, vertex.size = 3, vertex.label=NA)
