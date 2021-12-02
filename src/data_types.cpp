5c965b9b53d208a0f3760d61b52a1255302d5d35
Pratik Gupte <pratikgupte16@gmail.com>
2021-12-02 15:45:14 +0100

    def fun to log movement

--- src/data_types.cpp
+++ src/data_types.cpp
@@ -56,3 +56,31 @@ Rcpp::List genData::getGenData() {
 
     return dataToReturn;
 }
+
+void moveData::updateMoveData(Population &pop, const int t_) {
+    assert(t_ <= tmax && "too many timesteps logged");
+
+    timesteps[t_] = std::vector<int> (popsize, t_);
+    x[t_] = pop.coordX;
+    y[t_] = pop.coordX;
+
+    id_
+}
+
+Rcpp::List getMoveData() {
+    Rcpp::List mDataList (tmax);
+    std::vector<int> id(popsize, 0);
+    std::iota(std::begin(id), std::end(id), 0);
+
+    for (int i = 0; i < tmax; i++)
+    {
+        gDataList[i] = DataFrame::create(
+            Named("time") = timesteps[i],
+            Named("x") = x[i],
+            Named("y") = y[i],
+            Named("id") = id
+        );
+    }
+
+    return mDataList;
+}
