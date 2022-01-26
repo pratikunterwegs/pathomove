078375c6c09d98a5fab5e8554b751de12ce1d987
Pratik Gupte <pratikgupte16@gmail.com>
2022-01-26 21:56:03 +0100

    update check on cluster file

--- scripts/check_package_cluster.R
+++ scripts/check_package_cluster.R
@@ -10,23 +10,25 @@ password = readLines("data/password")
 
 # prepare parameters
 pathomove::make_parameter_file(
-  scenario = 1,
+  scenario = 2,
   popsize = 500,
-  nItems = 2000,
+  nItems = 1800,
   landsize = 60,
-  nClusters = 30,
+  nClusters = 60,
   clusterSpread = 1,
-  tmax = 100,
-  genmax = 100,
-  range_food = 0.5,
+  tmax = 300,
+  genmax = 1000,
+  gen_init = 700,
+  range_food = 1,
   range_agents = 1,
+  range_move = 1,
   handling_time = 5,
-  regen_time = 20,
-  pTransmit = "0.001",
-  initialInfections = 2,
-  costInfect = 0.05,
+  regen_time = c(20, 50, 100),
+  pTransmit = "0.05",
+  initialInfections = 20,
+  costInfect = c(0.005, 0.01, 0.02),
   nThreads = 2,
-  replicates = 3,
+  replicates = 5,
   which_file = "data/parameters/parameters_test.csv"
 )
 
@@ -34,6 +36,7 @@ pathomove::make_parameter_file(
 pathomove::use_cluster(
   ssh_con = "p284074@peregrine.hpc.rug.nl",
   password = password, 
+  folder = "snevo",
   script = "scripts/do_sim_cluster.R", 
   template_job = "bash/main_job_maker.sh", 
   parameter_file = "data/parameters/parameters_test.csv"
