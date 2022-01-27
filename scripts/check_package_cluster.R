a1daa83ae1b763bdbb7d9c76e6a89ec291e06c3b
Pratik Gupte <pratikgupte16@gmail.com>
2022-01-27 12:10:34 +0100

    cluster needs nthreads 1

--- scripts/check_package_cluster.R
+++ scripts/check_package_cluster.R
@@ -27,7 +27,7 @@ pathomove::make_parameter_file(
   pTransmit = "0.05",
   initialInfections = 20,
   costInfect = c(0.005, 0.01, 0.02),
-  nThreads = 2,
+  nThreads = 1,
   replicates = 1,
   which_file = "data/parameters/parameters_test.csv"
 )
