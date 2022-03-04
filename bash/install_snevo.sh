68af04cae8d347511027cf0b661df02a4870cae8
Pratik Gupte <pratikgupte16@gmail.com>
2022-03-04 18:36:44 +0100

    update shell script

--- bash/install_snevo.sh
+++ bash/install_snevo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# script to install snevo on the peregrine cluster
+# script to install pathomove on the peregrine cluster
 
 ml load R/4.1.0-foss-2021a
 ml load Boost/1.76.0-GCC-10.3.0
--- bash/main_job_maker.sh
+++ bash/main_job_maker.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#SBATCH --time=00:10:00
+#SBATCH --time=05:00:00
 #SBATCH --partition=regular
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --job-name=snevo_simulation
+#SBATCH --job-name=pathomove_default
 #SBATCH --array=1-n_array
-#SBATCH --output=/home/p284074/snevo/data/output/snevo_output_%a.log
-#SBATCH --mem=5000
+#SBATCH --output=/home/p284074/patho-move-evol/data/output/pathomove_default_%a.log
+#SBATCH --mem=8000
 
 pwd
 ml load R/4.1.0-foss-2021a
 ml load Boost/1.76.0-GCC-10.3.0
-cd snevo
+cd patho-move-evol
 echo "now in" 
 pwd
 Rscript some rscript here ${SLURM_ARRAY_TASK_ID}
