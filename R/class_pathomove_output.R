15ea47b39355523578a65f4e7f228a480e77d4cb
Pratik Gupte <pratikgupte16@gmail.com>
2022-04-22 18:43:23 +0200

    check class before def

--- R/class_pathomove_output.R
+++ R/class_pathomove_output.R
@@ -1,3 +1,49 @@
+#' Checks the validity of the `pathomove_output` class.
+#'
+#' @param object Instance of the `pathomove_output` class.
+#'
+#' @return Boolean or errors
+#' @export
+#'
+check_pathomove_output <- function(object) {
+  errors <- character()
+  length_scenario <- length(object@scenario)
+  if (length_scenario != 1) {
+    msg <- sprintf("scenario is length %i, should be 1.", length_scenario)
+    errors <- c(errors, msg)
+  }
+  
+  # length_status <- length(object@status)
+  # if (length_status != 1) {
+  #   msg <- paste("status is length ", length_status, ". Should be 1", sep = "")
+  #   errors <- c(errors, msg)
+  # }
+  # 
+  # length_missing_species <- length(object@missing_species)
+  # if (length_missing_species != 1) {
+  #   msg <- paste(
+  #     "missing_species is length ", length_missing_species, ". Should be 1",
+  #     sep = ""
+  #   )
+  #   errors <- c(errors, msg)
+  # }
+  # 
+  # length_min_age <- length(object@min_age)
+  # if (length_min_age != 1) {
+  #   msg <- paste(
+  #     "min_age is length ", length_min_age, ". Should be 1",
+  #     sep = ""
+  #   )
+  #   errors <- c(errors, msg)
+  # }
+  
+  if (length(errors) == 0) {
+    TRUE
+  } else {
+    errors
+  }
+}
+
 #' Defines the `pathomove_output` class.
 #'
 #' @slot scenario integer.
@@ -29,49 +75,3 @@ setClass(
   # check validity of class
   validity = check_pathomove_output
 )
-
-#' Checks the validity of the `pathomove_output` class.
-#'
-#' @param object Instance of the `pathomove_output` class.
-#'
-#' @return Boolean or errors
-#' @export
-#'
-check_pathomove_output <- function(object) {
-  errors <- character()
-  length_scenario <- length(object@scenario)
-  if (length_scenario != 1) {
-    msg <- sprintf("scenario is length %i, should be 1.", length_scenario)
-    errors <- c(errors, msg)
-  }
-
-  # length_status <- length(object@status)
-  # if (length_status != 1) {
-  #   msg <- paste("status is length ", length_status, ". Should be 1", sep = "")
-  #   errors <- c(errors, msg)
-  # }
-  # 
-  # length_missing_species <- length(object@missing_species)
-  # if (length_missing_species != 1) {
-  #   msg <- paste(
-  #     "missing_species is length ", length_missing_species, ". Should be 1",
-  #     sep = ""
-  #   )
-  #   errors <- c(errors, msg)
-  # }
-  # 
-  # length_min_age <- length(object@min_age)
-  # if (length_min_age != 1) {
-  #   msg <- paste(
-  #     "min_age is length ", length_min_age, ". Should be 1",
-  #     sep = ""
-  #   )
-  #   errors <- c(errors, msg)
-  # }
-
-  if (length(errors) == 0) {
-    TRUE
-  } else {
-    errors
-  }
-}
