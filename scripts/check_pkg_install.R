2b19f9dfab70ee620578e13d71e5062d0e76a9fa
Pratik Gupte <pratikgupte16@gmail.com>
2022-04-23 16:01:42 +0200

    temp add s4 output to install check script

--- scripts/check_pkg_install.R
+++ scripts/check_pkg_install.R
@@ -172,3 +172,29 @@ g <- tidygraph::as_tbl_graph(g)
 ggraph(g, layout = "mds") +
   geom_node_point() +
   geom_edge_link()
+
+#### check S4 class output ####
+a = pathomove::run_pathomove_s4(
+  scenario = 2,
+  popsize = 10,
+  nItems = 180,
+  landsize = 10,
+  nClusters = 10,
+  clusterSpread = 1,
+  tmax = 100,
+  genmax = 10,
+  g_patho_init = 5,
+  range_food = 1.0,
+  range_agents = 1.0,
+  range_move = 1.0,
+  handling_time = 5,
+  regen_time = 50,
+  pTransmit = 0.05,
+  initialInfections = 4,
+  costInfect = 0.25,
+  nThreads = 2,
+  dispersal = 3.0,
+  infect_percent = FALSE,
+  mProb = 0.001,
+  mSize = 0.001
+)
