3910cdf3ed8f4c8060d6b64aa553a10ce3f38899
Pratik Gupte <pratikgupte16@gmail.com>
2022-04-23 19:14:11 +0200

    update slots

--- R/class_pathomove_output.R
+++ R/class_pathomove_output.R
@@ -12,7 +12,7 @@ check_pathomove_output <- function(object) {
     msg <- sprintf("scenario is length %i, should be 1.", length_scenario)
     errors <- c(errors, msg)
   }
-  
+
   if (length(errors) == 0) {
     TRUE
   } else {
@@ -22,10 +22,10 @@ check_pathomove_output <- function(object) {
 
 #' Defines the `pathomove_output` class.
 #'
-#' @slot scenario integer.
-#' @slot n_gen integer.
-#' @slot gen_patho_intro integer.
+#' @slot parameters list.
+#' @slot generations integer.
 #' @slot infections_per_gen integer.
+#' @slot trait_data list.
 #'
 #' @export
 setClass(
@@ -35,14 +35,16 @@ setClass(
   # define the types of the class
   slots = c(
     parameters = "list",
-    infections_per_gen = "data.frame",
+    generations = "integer",
+    infections_per_gen = "integer",
     trait_data = "list"
   ),
 
   # define the default values of the slots
   prototype = list(
     parameters = list(),
-    infections_per_gen = data.frame(),
+    generations = NA_integer_,
+    infections_per_gen = NA_integer_,
     trait_data = list()
   ),
 
