3341ca753943d926ee6eb2daafee9b95f3853a3c
Pratik Gupte <pratikgupte16@gmail.com>
2022-06-15 19:28:21 +0200

    convert function to s4 return with params

--- src/simulations.cpp
+++ src/simulations.cpp
@@ -31,6 +31,9 @@ Rcpp::List simulation::do_simulation() {
     Rcpp::Rcout << "initialised population positions\n";
     Rcpp::List edgeLists;
 
+    // vector to hold generations in which edgelists are logged
+    std::vector<int> gens_edge_lists;
+
     Rcpp::Rcout << "created edge list object\n";
 
     // agent data logging increment
@@ -122,6 +125,7 @@ Rcpp::List simulation::do_simulation() {
 
         if((gen == 0) | ((gen % (genmax / 10)) == 0) | (gen == genmax - 1)) {
             edgeLists.push_back(pop.pbsn.getNtwkDf());
+            gens_edge_lists.push_back(gen);
             Rcpp::Rcout << "gen: " << gen << " --- logged edgelist\n";
         }
 
@@ -137,93 +141,10 @@ Rcpp::List simulation::do_simulation() {
     return Rcpp::List::create(
         Named("gen_data") = gen_data.getGenData(),
         Named("edgeLists") = edgeLists,
+        Named("gens_edge_lists") = gens_edge_lists,
         Named("move_pre") = mdPre.getMoveData(),
         Named("move_post") = mdPost.getMoveData()
     );
-}
-
-//' Runs the pathomove simulation.
-//'
-//' @description Run the simulation using parameters passed as
-//' arguments to the corresponding R function.
-//'
-//' @param scenario The pathomove scenario: 0 for no pathogen, 1 for 
-//' persistent introduction across generations, and 2 for a single introduction.
-//' @param popsize The population size.
-//' @param nItems How many food items on the landscape.
-//' @param landsize The size of the landscape as a numeric (double).
-//' @param nClusters Number of clusters around which food is generated.
-//' @param clusterSpread How dispersed food is around the cluster centre.
-//' @param tmax The number of timesteps per generation.
-//' @param genmax The maximum number of generations per simulation.
-//' @param g_patho_init The generation in which to begin introducing the pathogen.
-//' @param range_food The sensory range for food.
-//' @param range_agents The sensory range for agents.
-//' @param range_move The movement range for agents.
-//' @param handling_time The handling time.
-//' @param regen_time The item regeneration time.
-//' @param pTransmit Probability of transmission.
-//' @param initialInfections Agents infected per event.
-//' @param costInfect The per-timestep cost of pathogen infection.
-//' @param nThreads How many threads to parallelise over. Set to 1 to run on
-//' the HPC Peregrine cluster.
-//' @param dispersal A float value; the standard deviation of a normal
-//' distribution centred on zero, which determines how far away from its parent
-//' each individual is initialised. The standard value is 5 percent of the
-//' landscape size (\code{landsize}), and represents local dispersal.
-//' Setting this to 10 percent is already almost equivalent to global dispersal.
-//' @param infect_percent A boolean value; whether the infection depletes a
-//' percentage of daily energy (\code{TRUE}) or whether a fixed value 
-//' (\code{FALSE}) is subtracted from net energy.
-//' For \code{infect_percent = TRUE}, the net energy remaining after \code{T} 
-//' timesteps of infection is \code{N * (1 - cost_infect) ^ T}, where \code{N}
-//' is total intake.
-//' For \code{infect_percent = FALSE}, the net energy remaining after \code{T} 
-//' timesteps of infection is \code{N - (cost_infect * T)}, where \code{N}
-//' is total intake.
-//' @param mProb The probability of mutation. The suggested value is 0.01.
-//' While high, this may be more appropriate for a small population; change this
-//' value and \code{popsize} to test the simulation's sensitivity to these values.
-//' @param mSize Controls the mutational step size, and represents the scale
-//' parameter of a Cauchy distribution. 
-//' @return A data frame of the evolved population traits.
-// [[Rcpp::export]]
-Rcpp::List run_pathomove(const int scenario,
-                        const int popsize,
-                        const int nItems, const float landsize,
-                        const int nClusters,
-                        const float clusterSpread,
-                        const int tmax,
-                        const int genmax,
-                        const int g_patho_init,
-                        const float range_food,
-                        const float range_agents,
-                        const float range_move,
-                        const int handling_time,
-                        const int regen_time,
-                        float pTransmit,
-                        const int initialInfections,
-                        const float costInfect,
-                        const int nThreads,
-                        const float dispersal,
-                        const bool infect_percent,
-                        const bool vertical,
-                        const float mProb,
-                        const float mSize) {
-                            
-    // check that intial infections is less than popsize
-    if(initialInfections > popsize) {
-        Rcpp::stop("More infections than agents!");
-    }
-                           
-    simulation this_sim(popsize, scenario, nItems, landsize,
-                        nClusters, clusterSpread, tmax, genmax, g_patho_init,
-                        range_food, range_agents, range_move,
-                        handling_time, regen_time,
-                        pTransmit, initialInfections, 
-                        costInfect, nThreads, dispersal, infect_percent, vertical,
-                        mProb, mSize);
-    return this_sim.do_simulation();
 }
 
 //' Runs the pathomove simulation and return a `pathomove_output` object.
@@ -274,28 +195,28 @@ Rcpp::List run_pathomove(const int scenario,
 //' parameter of a Cauchy distribution. 
 //' @return An S4 class, `pathomove_output`, with simulation outcomes.
 // [[Rcpp::export]]
-S4 run_pathomove_s4(const int scenario,
-                        const int popsize,
-                        const int nItems, const float landsize,
-                        const int nClusters,
-                        const float clusterSpread,
-                        const int tmax,
-                        const int genmax,
-                        const int g_patho_init,
-                        const float range_food,
-                        const float range_agents,
-                        const float range_move,
-                        const int handling_time,
-                        const int regen_time,
-                        float pTransmit,
-                        const int initialInfections,
-                        const float costInfect,
-                        const int nThreads,
-                        const float dispersal,
-                        const bool infect_percent,
-                        const bool vertical,
-                        const float mProb,
-                        const float mSize) {
+S4 run_pathomove(const int scenario,
+                const int popsize,
+                const int nItems, const float landsize,
+                const int nClusters,
+                const float clusterSpread,
+                const int tmax,
+                const int genmax,
+                const int g_patho_init,
+                const float range_food,
+                const float range_agents,
+                const float range_move,
+                const int handling_time,
+                const int regen_time,
+                float pTransmit,
+                const int initialInfections,
+                const float costInfect,
+                const int nThreads,
+                const float dispersal,
+                const bool infect_percent,
+                const bool vertical,
+                const float mProb,
+                const float mSize) {
 
     // check that intial infections is less than popsize
     if(initialInfections > popsize) {
@@ -318,48 +239,71 @@ S4 run_pathomove_s4(const int scenario,
     Rcpp::List pop_data = gen_data["pop_data"];
 
     // return scenario as string
-    std::string scenario_str;
+    Rcpp::String scenario_str("scenario_here");
     switch (scenario)
     {
     case 0:
-        scenario_str = std::string("no pathogen");
+        scenario_str = "no pathogen";
         Rcpp::Rcout << "No pathogen introduction\n";
         break;
     case 1:
-        if(g_patho_init == 0) scenario_str = std::string("endemic pathogen");
-        else scenario_str = std::string("novel pathogen");
+        if(g_patho_init == 0) scenario_str = "endemic pathogen";
+        else scenario_str = "novel pathogen";
         Rcpp::Rcout << "Pathogen introduced from gen:" << g_patho_init << "\n";
         break;
     case 2:
-        scenario_str = std::string("single spillover");
+        scenario_str = "single spillover";
         break;
     default:
-        scenario_str = std::string("unknown scenario");
+        scenario_str = "unknown scenario";
         break;
     }
 
-    std::string infection_cost_type = infect_percent ? std::string("percent") :
-        std::string("absolute");
+    Rcpp::String infection_cost_type = infect_percent ? "percent" :
+        "absolute";
 
-    // parameter list
-    Rcpp::List param_list = Rcpp::List::create(
-            Named("scenario") = scenario_str,
-            Named("generations") = genmax,
-            Named("gen_patho_intro") = (scenario == 0 ? NA_REAL : g_patho_init),
-            Named("pop_size") = popsize,
-            Named("pop_density") = static_cast<float>(popsize) / landsize,
-            Named("item_density") = static_cast<float>(nItems) / landsize,
-            Named("dispersal") = dispersal,
-            Named("infection_cost_implementation") = infection_cost_type,
-            Named("infection_cost") = costInfect
-        );
+    Rcpp::String vertical_infection = vertical ? "vertical" :
+        "no_vertical";
+    
+    // agents parameter list
+    Rcpp::List agents_param_list = Rcpp::List::create(
+        Named("popsize") = popsize,
+        Named("range_food") = range_food,
+        Named("range_agents") = range_agents,
+        Named("range_move") = range_move,
+        Named("handling_time") = handling_time,
+        Named("pTransmit") = pTransmit,
+        Named("initialInfections") = initialInfections,
+        Named("costInfect") = costInfect,
+        Named("dispersal") = dispersal,
+        Named("mProb") = mProb,
+        Named("mSize") = mSize
+    );
+
+    // ecological parameters list
+    Rcpp::List eco_param_list = Rcpp::List::create(
+        Named("scenario") = scenario_str,
+        Named("infect_percent") = infection_cost_type,
+        Named("vertical_infection") = vertical_infection,
+        Named("genmax") = genmax,
+        Named("g_patho_init") = (scenario == 0 ? NA_REAL : g_patho_init),
+        Named("nItems") = nItems,
+        Named("landsize") = landsize,
+        Named("nClusters") = nClusters,
+        Named("clusterSpread") = clusterSpread,
+        Named("tmax") = tmax,
+        Named("regen_time") = regen_time
+    );
 
     // create S4 class pathomove output and fill slots
     S4 x("pathomove_output");
-    x.slot("parameters") = Rcpp::wrap(param_list);
+    x.slot("agent_parameters") = Rcpp::wrap(agents_param_list);
+    x.slot("eco_parameters") = Rcpp::wrap(eco_param_list);
     x.slot("generations") = Rcpp::wrap(gen_data["gens"]);
     x.slot("infections_per_gen") = Rcpp::wrap(gen_data["n_infected"]);
     x.slot("trait_data") = Rcpp::wrap(pop_data);
+    x.slot("edge_lists") = Rcpp::wrap(pathomoveOutput["edgeLists"]);
+    x.slot("gens_edge_lists") = Rcpp::wrap(pathomoveOutput["gens_edge_lists"]);
 
     return(x);
 }
