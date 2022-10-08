73f00486f8c032f5d9405c94f2a268c247e9dd8d
Pratik Gupte <pratikgupte16@gmail.com>
2022-10-09 00:01:20 +0100

    WIP landscape; replace std RNG

--- src/landscape.cpp
+++ src/landscape.cpp
@@ -11,48 +11,40 @@ using namespace Rcpp;
 
 #include "landscape.h"
 
-std::mt19937 rng;
+boost::mt19937 gen;
 
 void Resources::initResources() {
   // generate n central items
   std::vector<float> centreCoordX(nClusters);
   std::vector<float> centreCoordY(nClusters);
 
-  std::uniform_real_distribution<float> item_ran_pos(0.0f, dSize);
-  std::normal_distribution<float> item_cluster_spread(0.0f, clusterSpread);
+  // make item and cluster coords
+  Rcpp::NumericVector cluster_rd_x = Rcpp::runif(nClusters, 0.0f, dSize);
+  Rcpp::NumericVector cluster_rd_y = Rcpp::runif(nClusters, 0.0f, dSize);
+
+  Rcpp::NumericVector rd_x = Rcpp::runif(nItems, 0.0f, clusterSpread);
+  Rcpp::NumericVector rd_y = Rcpp::runif(nItems, 0.0f, clusterSpread);
 
   for (size_t i = 0; i < static_cast<size_t>(nClusters); i++) {
-    centreCoordX[i] = item_ran_pos(rng);
-    centreCoordY[i] = item_ran_pos(rng);
-
-    // also add to main set
-    coordX[i] = centreCoordX[i];
-    coordY[i] = centreCoordY[i];
+    centreCoordX[i] = cluster_rd_x(i);
+    centreCoordY[i] = cluster_rd_y(i);
   }
 
   // generate items around
   for (int i = nClusters; i < nItems; i++) {
-    coordX[i] = (centreCoordX[(i % nClusters)] + item_cluster_spread(rng));
-    coordY[i] = (centreCoordY[(i % nClusters)] + item_cluster_spread(rng));
+    coordX[i] = (centreCoordX[(i % nClusters)] + rd_x(i));
+    coordY[i] = (centreCoordY[(i % nClusters)] + rd_y(i));
 
     // wrap
     coordX[i] = fmod(dSize + coordX[i], dSize);
     coordY[i] = fmod(dSize + coordY[i], dSize);
   }
 
-  // dist to set random counter value
-  std::poisson_distribution<int> distRegen(
-      static_cast<int>(std::floor(static_cast<float>(regen_time) * 0.1)));
-
   // initialise rtree and set counter value
   bgi::rtree<value, bgi::quadratic<16>> tmpRtree;
   for (int i = 0; i < nItems; ++i) {
     point p = point(coordX[i], coordY[i]);
     tmpRtree.insert(std::make_pair(p, i));
-
-    counter[i] = distRegen(rng);
-    // set all to available
-    available[i] = (counter[i] == 0);
   }
 
   std::swap(rtree, tmpRtree);
@@ -87,6 +79,7 @@ void Resources::regenerate() {
 //' @param clusterSpread Dispersal of items around cluster centres.
 //' @param regen_time Regeneration time, in timesteps.
 //' @return A data frame of the evolved population traits.
+//' @export
 // [[Rcpp::export]]
 Rcpp::DataFrame get_test_landscape(const int &nItems, const float &landsize,
                                    const int &nClusters,
@@ -94,12 +87,11 @@ Rcpp::DataFrame get_test_landscape(const int &nItems, const float &landsize,
                                    const int &regen_time) {
   unsigned seed = static_cast<unsigned>(
       std::chrono::system_clock::now().time_since_epoch().count());
-  rng.seed(seed);
+  gen.seed(seed);
 
   Resources food(nItems, landsize, nClusters, clusterSpread, regen_time);
   food.initResources();
 
   return Rcpp::DataFrame::create(Rcpp::Named("x") = food.coordX,
-                                 Rcpp::Named("y") = food.coordY,
-                                 Rcpp::Named("tAvail") = food.counter);
+                                 Rcpp::Named("y") = food.coordY);
 }
