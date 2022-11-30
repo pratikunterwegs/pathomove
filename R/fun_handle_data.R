cea143770217740885970aac4dbe8e9d794e4d96
Pratik Gupte <pratikgupte16@gmail.com>
2022-11-30 16:45:26 +0000

    Function to get movement data

--- R/fun_handle_data.R
+++ R/fun_handle_data.R
@@ -61,3 +61,32 @@ get_trait_data <- function(object,
 
   return(trait_data_)
 }
+
+#' Get sample movement data from _pathomove_ output.
+#'
+#' @param object A `pathomove_output` object.
+#'
+#' @return A `data.table` with the positions of all individuals in two
+#' generations: "pre", for the generation immediately before pathogen
+#' introduction (`g_patho_init` - 1 in `run_pathomove`); and "post", for the
+#' last generation of the simulation (`genmax` - 1 in `run_pathomove`).
+#' @export
+#'
+get_move_data <- function(object) {
+  assertthat::assert_that(
+    class(object) == "pathomove_output",
+    msg = "Object must be of the class `pathomove_output`"
+  )
+
+  move_data <- object@move_data
+  move_data <- Map(
+    move_data,
+    names(move_data),
+    f = function(le, name) {
+      df_ <- data.table::rbindlist(le)
+      df_$type <- name
+      df_
+    }
+  )
+  data.table::rbindlist(move_data)
+}
