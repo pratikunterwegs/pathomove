ade9a2dbec62b9fba471f272bc50678868e49923
Pratik Gupte <pratikgupte16@gmail.com>
2022-11-30 14:40:34 +0000

    Add check and run script

---
+++ inst/scripts/check_package_cluster.R
@@ -0,0 +1,37 @@
+
+library(pathomove)
+password <- readLines("data/password")
+
+# prepare parameters
+pathomove::make_parameter_file(
+  scenario = 2,
+  popsize = 500,
+  nItems = 1800,
+  landsize = 60,
+  nClusters = 60,
+  clusterSpread = 1,
+  tmax = 300,
+  genmax = 1000,
+  g_patho_init = 700,
+  range_food = 1,
+  range_agents = 1,
+  range_move = 1,
+  handling_time = 5,
+  regen_time = c(20, 50, 100),
+  pTransmit = "0.05",
+  initialInfections = 20,
+  costInfect = c(0.005, 0.01, 0.02),
+  multithreaded = FALSE,
+  replicates = 1,
+  which_file = "data/parameters/parameters_test.csv"
+)
+
+# try sending in a job
+pathomove::use_cluster(
+  ssh_con = "p284074@peregrine.hpc.rug.nl",
+  password = password,
+  folder = "snevo",
+  script = "scripts/do_sim_cluster.R",
+  template_job = "bash/main_job_maker.sh",
+  parameter_file = "data/parameters/parameters_test.csv"
+)
---
+++ inst/scripts/do_sim_pathomove.R
@@ -0,0 +1,71 @@
+args <- commandArgs(TRUE)
+
+message(getwd())
+
+param_file <- args[1]
+
+row_n <- as.numeric(args[2])
+
+message(
+  paste("which file = ", param_file)
+)
+
+message(
+  paste("which row = ", row_n)
+)
+
+params <- read.csv(param_file)
+
+library(pathomove)
+
+# run simulation
+data <- pathomove::run_pathomove(
+  scenario = params$scenario[row_n],
+  popsize = params$popsize[row_n],
+  nItems = params$popsize[row_n],
+  landsize = params$landsize[row_n],
+  nClusters = params$nClusters[row_n],
+  clusterSpread = params$clusterSpread[row_n],
+  tmax = params$tmax[row_n],
+  genmax = params$genmax[row_n],
+  g_patho_init = params$g_patho_init[row_n],
+  range_food = params$range_food[row_n],
+  range_agents = params$range_agents[row_n],
+  range_move = params$range_move[row_n],
+  handling_time = params$handling_time[row_n],
+  regen_time = params$regen_time[row_n],
+  pTransmit = params$pTransmit[row_n],
+  initialInfections = params$initialInfections[row_n],
+  costInfect = params$costInfect[row_n],
+  multithreaded = params$multithreaded[row_n],
+  dispersal = params$dispersal[row_n],
+  infect_percent = params$infect_percent[row_n]
+)
+
+# get params as named vector
+these_params <- unlist(params[row_n, ])
+
+# append list of params
+data <- append(
+  data,
+  these_params
+)
+
+output_file_index <- params$ofi[row_n]
+
+sc_type <- dplyr::case_when(
+  params$scenario[row_n] == 0 ~ "nopatho",
+  params$scenario[row_n] == 1 ~ "endemic",
+  params$scenario[row_n] == 2 ~ "spillover"
+)
+
+# name of rdata file
+output_file <- glue::glue(
+  "data/output/scenario_{sc_type}_{output_file_index}.Rds"
+)
+
+# save
+save(
+  data,
+  file = output_file
+)
