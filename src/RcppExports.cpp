f6947a3dfe25e4d91488254caacb04e80576a5b9
Pratik Gupte <pratikgupte16@gmail.com>
2023-01-06 08:42:08 +0000

    Add package infrastructure

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -26,8 +26,8 @@ BEGIN_RCPP
 END_RCPP
 }
 // run_pathomove
-Rcpp::S4 run_pathomove(const int scenario, const int popsize, const int nItems, const float landsize, const int nClusters, const float clusterSpread, const int tmax, const int genmax, const int g_patho_init, const int n_samples, const float range_food, const float range_agents, const float range_move, const int handling_time, const int regen_time, float pTransmit, const float p_v_transmit, const int initialInfections, const float costInfect, const bool multithreaded, const float dispersal, const bool infect_percent, const bool vertical, const bool reprod_threshold, const float mProb, const float mSize, const float spillover_rate);
-RcppExport SEXP _pathomove_run_pathomove(SEXP scenarioSEXP, SEXP popsizeSEXP, SEXP nItemsSEXP, SEXP landsizeSEXP, SEXP nClustersSEXP, SEXP clusterSpreadSEXP, SEXP tmaxSEXP, SEXP genmaxSEXP, SEXP g_patho_initSEXP, SEXP n_samplesSEXP, SEXP range_foodSEXP, SEXP range_agentsSEXP, SEXP range_moveSEXP, SEXP handling_timeSEXP, SEXP regen_timeSEXP, SEXP pTransmitSEXP, SEXP p_v_transmitSEXP, SEXP initialInfectionsSEXP, SEXP costInfectSEXP, SEXP multithreadedSEXP, SEXP dispersalSEXP, SEXP infect_percentSEXP, SEXP verticalSEXP, SEXP reprod_thresholdSEXP, SEXP mProbSEXP, SEXP mSizeSEXP, SEXP spillover_rateSEXP) {
+Rcpp::S4 run_pathomove(const int scenario, const int popsize, const int nItems, const float landsize, const int nClusters, const float clusterSpread, const int tmax, const int genmax, const int g_patho_init, const int n_samples, const float range_food, const float range_agents, const float range_move, const int handling_time, const int regen_time, float pTransmit, const float p_v_transmit, const int initialInfections, const float costInfect, const bool multithreaded, const float dispersal, const bool infect_percent, const bool vertical, const bool reprod_threshold, const float mProb, const float mSize, const float spillover_rate, const int seed);
+RcppExport SEXP _pathomove_run_pathomove(SEXP scenarioSEXP, SEXP popsizeSEXP, SEXP nItemsSEXP, SEXP landsizeSEXP, SEXP nClustersSEXP, SEXP clusterSpreadSEXP, SEXP tmaxSEXP, SEXP genmaxSEXP, SEXP g_patho_initSEXP, SEXP n_samplesSEXP, SEXP range_foodSEXP, SEXP range_agentsSEXP, SEXP range_moveSEXP, SEXP handling_timeSEXP, SEXP regen_timeSEXP, SEXP pTransmitSEXP, SEXP p_v_transmitSEXP, SEXP initialInfectionsSEXP, SEXP costInfectSEXP, SEXP multithreadedSEXP, SEXP dispersalSEXP, SEXP infect_percentSEXP, SEXP verticalSEXP, SEXP reprod_thresholdSEXP, SEXP mProbSEXP, SEXP mSizeSEXP, SEXP spillover_rateSEXP, SEXP seedSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -58,7 +58,8 @@ BEGIN_RCPP
     Rcpp::traits::input_parameter< const float >::type mProb(mProbSEXP);
     Rcpp::traits::input_parameter< const float >::type mSize(mSizeSEXP);
     Rcpp::traits::input_parameter< const float >::type spillover_rate(spillover_rateSEXP);
-    rcpp_result_gen = Rcpp::wrap(run_pathomove(scenario, popsize, nItems, landsize, nClusters, clusterSpread, tmax, genmax, g_patho_init, n_samples, range_food, range_agents, range_move, handling_time, regen_time, pTransmit, p_v_transmit, initialInfections, costInfect, multithreaded, dispersal, infect_percent, vertical, reprod_threshold, mProb, mSize, spillover_rate));
+    Rcpp::traits::input_parameter< const int >::type seed(seedSEXP);
+    rcpp_result_gen = Rcpp::wrap(run_pathomove(scenario, popsize, nItems, landsize, nClusters, clusterSpread, tmax, genmax, g_patho_init, n_samples, range_food, range_agents, range_move, handling_time, regen_time, pTransmit, p_v_transmit, initialInfections, costInfect, multithreaded, dispersal, infect_percent, vertical, reprod_threshold, mProb, mSize, spillover_rate, seed));
     return rcpp_result_gen;
 END_RCPP
 }
@@ -67,7 +68,7 @@ RcppExport SEXP run_testthat_tests(SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
     {"_pathomove_get_test_landscape", (DL_FUNC) &_pathomove_get_test_landscape, 5},
-    {"_pathomove_run_pathomove", (DL_FUNC) &_pathomove_run_pathomove, 27},
+    {"_pathomove_run_pathomove", (DL_FUNC) &_pathomove_run_pathomove, 28},
     {"run_testthat_tests", (DL_FUNC) &run_testthat_tests, 1},
     {NULL, NULL, 0}
 };
